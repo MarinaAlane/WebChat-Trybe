a95d107841a321e91d50e95bea2f680309da9977
Marcelo <marsleite@gmail.com>
2021-12-07 16:49:28 -0300

    estrutura do websocket e conexão do front e back

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'html');
+
+app.use('/', (_req, res) => {
+  res.render('index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log(`Socket ${socket.id} connected`);
+
+  socket.on('message', (data) => {
+    console.log(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
