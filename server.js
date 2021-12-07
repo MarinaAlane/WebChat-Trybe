fd4b77ffcbd72beed1546e72bed7fdd9d90be3ac
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-07 15:55:15 -0300

    estrutura server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-// Faça seu código aqui
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+http.listen(3000, () => {
+  console.log('Servidor on na porta 3000');
+});
