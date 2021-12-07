955b99151e1b71bae17ef20b8e9229dc4383fcb7
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-12-07 15:43:00 -0300

    feat: config server to work with html file

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Faça seu código aqui
+// ..source: https://socket.io/get-started/chat
+
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (_req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
