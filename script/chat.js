95b2bbb694fd7f7730bc9b366afcca5ebe583610
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-12-07 15:23:28 -0300

    Add server, html and chat script

---
+++ script/chat.js
@@ -0,0 +1,36 @@
+// const io = require('socket.io');
+const socket = io('http://localhost:3000');
+
+let user = null;
+
+function updateMessagesOnScreen(messages) {
+  const divMessages = document.querySelector('#messages');
+
+  let listMessages = '<ul>';
+  messages.forEach((message) => {
+    listMessages += `<li>${message.user}: ${message.msg}</li>`;
+  });
+  listMessages += '</ul>';
+
+  divMessages.innerHTML = listMessages;
+}
+socket.on('update_messages', (messages) => {
+  updateMessagesOnScreen(messages);
+});
+
+const userForm = document.querySelector('#user_form');
+userForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  user = document.forms.user_form_name.user.value;
+  userForm.parentNode.removeChild(userForm);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#message_form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
+  const message = document.forms.message_form_name.msg.value;
+  document.forms.message_form_name.value = '';
+  socket.emit('new_message', { user, msg: message });
+});
--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+const socketIo = require('socket.io');
+
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+const port = 3000;
+
+const server = app.listen(port, () => console.log(`Listening port ${port}`));
+
+const messages = [];
+
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('new connection');
+  socket.emit('update_messages', messages);
+
+  socket.on('new_message', (data) => {
+    messages.push(data);
+    socket.enit('update_messages', messages);
+  });
+});
