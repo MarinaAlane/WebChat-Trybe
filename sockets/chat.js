4689966b9ed0be2926e020d148dc64fff97c12f6
Matheus Alexandre <alexandremhm@gmail.com>
2021-12-07 15:09:47 -0300

    Initial commit

---
+++ sockets/chat.js
@@ -0,0 +1,12 @@
+module.exports = (io) => io.on('connection', (socket) => {
+  socket.broadcast.emit('serverMessage', `Iiiiiirraaaa! ${socket.id} acabou de se conectar :D`);
+
+   socket.on('disconnect', () => {
+    socket.broadcast.emit('serverMessage', `Xiii! ${socket.id} acabou de se desconectar! :(`);
+  });
+
+  socket.on('clientMessage', (message) => {    
+    console.log(`Mensagem ${message}`);
+    io.emit('serverMessage', message);
+  });
+});
