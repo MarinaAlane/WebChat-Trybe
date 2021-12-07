903938e3f3cecd52db6256f8035396edba257c04
heyset <c.inaciomatheus@gmail.com>
2021-12-07 18:06:02 -0300

    Adiciona server com http e socket

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 require('dotenv').config();
+const socketio = require('socket.io');
+const http = require('http');
 const { app } = require('./app');
+
+const server = http.createServer(app);
+
+const io = socketio(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
 });
