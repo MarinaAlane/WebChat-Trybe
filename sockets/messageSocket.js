d2271504df43ca109bee2fa433ca94a0ed0f6de3
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-12-07 18:46:29 -0300

    feat: add setUsername emitter

--- sockets/messageSocket.js
+++ sockets/messageSocket.js
@@ -2,6 +2,8 @@ const moment = require('moment');
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
+    socket.emit('setUsername', socket.id.slice(-16));
+
     socket.on('message', ({ chatMessage, nickname }) => {
       const date = moment();
       const formatedDate = date.format('DD-MM-yyyy hh:mm:ss A');
