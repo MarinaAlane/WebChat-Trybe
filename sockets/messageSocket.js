df0db350f6fa7f552663bffc33377a517a04a880
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-12-07 18:17:18 -0300

    feat: add sendMessage reciever

--- sockets/messageSocket.js
+++ sockets/messageSocket.js
@@ -1,7 +1,12 @@
+const moment = require('moment');
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.on('sendMessage', (data) => {
-      console.log(data);
+    socket.on('sendMessage', ({ chatMessage, nickname }) => {
+      const date = moment();
+      const formatedDate = date.format('DD-MM-yyyy hh:mm:ss A');
+
+      socket.emit('message', `${formatedDate} - ${nickname}: ${chatMessage}`);
     });
   });
 };
