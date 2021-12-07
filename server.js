fd993dc3a7c4ec0c9e58861d8b4229383f654a7a
Aline <aline.melryaa@gmail.com>
2021-12-07 19:22:00 -0400

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 // Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+app.use(express.static(`${__dirname}/views`));
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
