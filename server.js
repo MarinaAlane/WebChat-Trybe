51b3f1b7e9344a6ee86556e140bfc56a474b2342
Leandro Lopes <leandro_carlos@uol.com.br>
2021-12-07 20:00:11 -0300

    conexao server e index.html

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-// Faça seu código aqui
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:3000',
+        method: ['GET', 'POST'],
+    },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    
+    socket.on('disconnect', () => {
+        console.log('user disconnected :(');
+    });
+    
+    socket.on('mensagem', (msg) => {
+        console.log(`alguem mandou uma mensagem: ${msg}`);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+http.listen(3000, () => {
+    console.log('listening on 3000');
+});
