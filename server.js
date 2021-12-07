ca5d5de6a1c0c8ab848013bbd6b683afeff05e32
heyset <c.inaciomatheus@gmail.com>
2021-12-07 17:58:46 -0300

    Adiciona server e app

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-// Faça seu código aqui
+require('dotenv').config();
+const { app } = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
+});
