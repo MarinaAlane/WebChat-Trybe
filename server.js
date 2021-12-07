0beff91fe276aea5f9664d020548b1685cb58733
Daniel <danielfdoficial@gmail.com>
2021-12-07 15:11:17 -0300

    requisito 1

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 // Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = 3000;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`, // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
+});
+
+require('./sockets/webchat')(io);
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => res.sendFile(`${__dirname}/public/webchat.html`));
+http.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}!`));
---
+++ sockets/webchat.js
@@ -0,0 +1,10 @@
+const moment = require('moment');
+
+const messageMoment = moment().format('MM-DD-YYYY h:mm:ss A');
+
+module.exports = (io) => io.on('connection', (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    console.log(`${messageMoment} - ${nickname}: ${chatMessage}`);
+    io.emit('message', `${messageMoment} - ${nickname}: ${chatMessage}`);
+  });
+});
