4bbf864649597ca09b87bbc017a739b3d56b4621
Amandha W. Barok <amandha.barok@gmail.com>
2021-12-07 11:00:31 -0800

    Starting the app

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-// Faça seu código aqui
+const express = require('express');
+const app = express();
+const http = require("http").createServer(app);
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT = 3000;
+
+app.use(bodyParser());
+app.use(cors());
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  }
+});
+
+app.use(express.static(__dirname + '/view'));
+
+require('./sockets/chat')(io);
+
+http.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
---
+++ view/js/chat.js
@@ -0,0 +1,22 @@
+const socket = window.io();
+
+const form = document.querySelector('form');
+const inputMessage = document.querySelector('#messageInput');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  socket.emit('clientMessage', inputMessage.value);
+  inputMessage.value = '';
+  return false;
+});
+
+const createMessage = (message) => {
+  const messagesUl = document.querySelector('#messages');
+  const li = document.createElement('li');
+  li.innerText = message;
+  messagesUl.appendChild(li);
+}
+
+socket.on('serverMessage', (mensagem) => createMessage(mensagem));
+
+// socket.on('pong', (mensagem) => createMessage(mensagem));
