dbae1d967a1a47fe1f65c618fc1d98dfb648b636
jamneto <netomoura22@gmail.com>
2021-12-07 16:13:15 -0300

    começando o projeto!

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+  res.status(200).json('Funfou');
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
