8d25130d60fa5103e78f057002a748a614c1c695
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-12-07 15:45:18 -0300

    feat: integrating socket.io

--- server.js
+++ server.js
@@ -2,12 +2,18 @@
 
 const express = require('express');
 const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (_req, res) => {
   res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 const PORT = process.env.PORT || 3000;
