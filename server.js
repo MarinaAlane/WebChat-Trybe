1e3937a3acc51c7d0005130b57a09abc60d5c7df
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-12-07 16:51:21 -0300

    cria configuracao inicial do server

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Faça seu código aqu
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  }});
+
+  require('./sockets/chat')(io);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
