99e51e9ea8598f817083bab3dd1f7cd014c4a26f
Daniel <danielfdoficial@gmail.com>
2021-12-07 17:22:15 -0300

    requisito 2

--- public/js/webchat.js
+++ public/js/webchat.js
@@ -1,20 +1,45 @@
 const socket = window.io();
 
-const form = document.querySelector('form');
+const formMessage = document.querySelector('#messageForm');
 const inputMessage = document.querySelector('#messageInput');
 
-form.addEventListener('submit', (e) => {
+const formNickname = document.querySelector('#nicknameForm');
+const inputNickname = document.querySelector('#nicknameInput');
+
+const userNickname = document.querySelector('#nickname');
+
+formMessage.addEventListener('submit', (e) => {
   e.preventDefault();
-  socket.emit('message', { chatMessage: inputMessage.value, nickname: 'fulano' });
+  socket.emit('message', { chatMessage: inputMessage.value, nickname: userNickname.innerText });
   inputMessage.value = '';
   return false;
 });
 
+formNickname.addEventListener('submit', (e) => {
+  e.preventDefault();
+  userNickname.innerText = inputNickname.value;
+  inputNickname.value = '';
+});
+
 const createMessage = (message) => {
+  console.log(socket.id);
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
+  li.setAttribute('data-testid', 'message');
   li.innerText = message;
   messagesUl.appendChild(li);
 };
+
+const cutID = (id) => id.slice(-16);
+
+const showUserNickname = () => {
+  socket.emit('generateNickname');
+
+  socket.on('generateNickname', (id) => {
+    userNickname.innerText = cutID(id);
+  });
+};
+
+showUserNickname();
 
 socket.on('message', (message) => createMessage(message));
--- sockets/webchat.js
+++ sockets/webchat.js
@@ -3,8 +3,13 @@ const moment = require('moment');
 const messageMoment = moment().format('MM-DD-YYYY h:mm:ss A');
 
 module.exports = (io) => io.on('connection', (socket) => {
+  const { id } = socket;
   socket.on('message', ({ chatMessage, nickname }) => {
     console.log(`${messageMoment} - ${nickname}: ${chatMessage}`);
     io.emit('message', `${messageMoment} - ${nickname}: ${chatMessage}`);
+  });
+
+  socket.on('generateNickname', () => {
+    socket.emit('generateNickname', id);
   });
 });
