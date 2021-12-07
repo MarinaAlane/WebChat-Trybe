90179a80109cbe9ef6c489e286a4e3f37ac4a3bc
Filizzola <juliofilizzola@hotmail.com>
2021-12-07 17:53:28 -0300

    BUILD: deletando

---
+++ public/script/index.js
@@ -0,0 +1,57 @@
+const socket = window.io('http://localhost:3000');
+
+let userNickname = null;
+
+const updateMessagesOnScreen = (messages) => {
+  const divMessages = document.querySelector('#messages');
+ 
+  let listMessages = '<ul>';
+  if (!messages) return;
+
+  messages.forEach(({ msg }) => {
+    listMessages += `<li>${msg}</li>`;
+  });
+  listMessages += '</ul>';
+
+  divMessages.innerHTML = listMessages;
+};
+
+const updateUserOnScreen = (users) => {
+  const divUser = document.querySelector('.user');
+
+  let listUser = '<ul>';
+  if (!users) return;
+
+  users.forEach(({ user }) => {
+    listUser += `<li>${user}</li>`;
+  });
+  listUser += '</ul>';
+
+ divUser.innerHTML = listUser;
+};
+
+socket.on('update_messages', (messages) => {
+  console.log(messages)
+  updateMessagesOnScreen(messages);
+  updateUserOnScreen(messages);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#message_form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const message = document.forms.message_form_name.msg.value;
+  
+    document.forms.message_form_name.msg.value = '';
+    socket.emit('new_message', { user: userNickname, msg: message });
+  });
+
+  const userForm = document.querySelector('#user_form');
+
+  userForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    userNickname = document.forms.user_form_name.user.value;
+  });
+});
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
 const path = require('path');
@@ -6,16 +7,18 @@ const socketIo = require('socket.io');
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-const serve = app.listen(port, () => console.log(`Example app listening on port %s`, port));
+const serve = app.listen(port, () => console.log('Example app listening on port %s', port));
 
 const io = socketIo(serve);
 
+const messages = [];
+
 io.on('connection', (socket) => {
-  console.log("new connection");
-  socket.emit('update_messages', getAllMessages());
+  console.log('new connection');
+  io.emit('update_messages', messages);
   socket.on('new_message', (data) => {
-  
-  
-    socket.emit('update_messages', getAllMessages());
+    console.log(data)
+    messages.push(data);
+    io.emit('update_messages', messages);
   });
 });
