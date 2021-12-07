8e289e0be514bdb556b634e3efaca70a36f6b22c
Ghislaine <glenox79@gmail.com>
2021-12-07 15:04:54 -0300

    Inicializando aplicacao no server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Listening in ${PORT}`));
