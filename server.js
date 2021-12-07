7169fab1436973b889e489a3040ddb9207230daf
Felipe Buso <felipe_buso@hotmail.com>
2021-12-07 17:11:01 -0300

    iniciando o projeto :rocket:

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// Faça seu código aqui
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello word');
+});
+
+app.listen(3000, console.log(`Escutando a porta ${PORT}`));
