ae6077e77328707d584898b8979de76f12d66211
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-12-07 19:03:41 -0300

    Req01, configuraçao do server

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-/* // Faça seu código aqui 
+// Faça seu código aqui 
 const express = require('express');
+const moment = require('moment');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
+const http = require('http').createServer(app);
 
-const PORT = process.env.PORT || 27017;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Ouvindo a porta ${PORT}`);
-}); */
+// configurar a entrada e saíde de dados
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`, // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  } });
+
+  app.use(express.static('views'));
+  app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, 'views'));
+
+io.on('connection', (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const date = moment().format('DD-MM-YYYY HH:mm:ss a');
+    io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
+  });
+});
+
+app.get('/', (_req, res) => {
+  res.render('index');
+});
+
+http.listen(PORT, () => {
+  console.log(`Servidor escutando na porta ${PORT}`);
+});
