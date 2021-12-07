1b4defb6c576d15d78d4fb05346a32c42588b23b
Marco Galindo <marco1408@gmail.com>
2021-12-07 15:06:24 -0300

    montando estrutura básica do projeto

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const path = require('path');
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  } });
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+require('./sockets/chat')(io);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
