96c6b73859d1184ce158f3331f1d9491221e5471
marlon307 <marlon-ramosb@hotmail.com>
2021-12-07 16:07:01 -0300

    Style finish

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Faça seu código aqui
+const express = require('express');
+
+const PORT = 3000;
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000/', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
+});
+
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
+app.listen(PORT, () => {
+  console.log(`Server started on http//:localhost:${PORT}`);
+});
