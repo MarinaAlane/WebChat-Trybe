421067cf43758015b74899a9a9e2e8d2bcb39da5
marlon307 <marlon-ramosb@hotmail.com>
2021-12-07 19:20:21 -0300

    Rq 1 finish

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,16 +3,20 @@ const socket = window.io();
 const nickName = document.getElementById('nickname-box');
 const messageBox = document.getElementById('message-box');
 const buttonMessage = document.getElementById('send-message');
+const listMessage = document.getElementById('list-message');
 
 buttonMessage.addEventListener('click', (event) => {
   event.preventDefault();
 
-  socket.emit('sendMessage', {
+  socket.emit('message', {
     nickname: nickName.value,
     chatMessage: messageBox.value,
   });
 });
 
-socket.on('resMessage', (res) => {
-  console.log(res);
+socket.on('message', async (renderMessage) => {
+  const itemListElement = document.createElement('li');
+  itemListElement.innerText = renderMessage;
+  itemListElement.setAttribute('data-testid', 'message');
+  listMessage.appendChild(itemListElement);
 });
--- sockets/socketChat.js
+++ sockets/socketChat.js
@@ -4,13 +4,8 @@ const time = moment().format('DD-MM-YYYY hh:mm:ss A');
 
 module.exports = (io) =>
   io.on('connection', (socket) => {
-    // console.log(socket);
-    socket.on('sendMessage', (message) => {
-      const formatMessage = {
-        time,
-        message,
-      };
-      console.log(formatMessage);
-      socket.emit('resMessage', formatMessage);
+    socket.on('message', async (message) => {
+      const formatMessage = `${time} - ${message.nickname}: ${message.chatMessage}`;
+      io.emit('message', formatMessage);
     });
   });
