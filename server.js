518f5e938432c527cf586457e7c19cd370dcd6bd
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-07 16:06:38 -0300

    adiciona index.html

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+
 const app = express();
 const http = require('http').createServer(app);
 
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  } });
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id} `);
+});
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(`${__dirname}/index.html`);
 });
 
 http.listen(3000, () => {
