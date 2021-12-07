b96f24c9a866ab22e4a781d9c483fbf52bf66c6e
marlon307 <marlon-ramosb@hotmail.com>
2021-12-07 17:52:05 -0300

    Socket responded server

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require('express');
 
 const PORT = 3000;
 const app = express();
-const http = require('http').createServer(app);
+const http = require('http');
 
-const io = require('socket.io')(http, {
-  cors: {
-    origin: 'http://localhost:3000/', // url aceita pelo cors
-    methods: ['GET', 'POST'], // Métodos aceitos pela url
-  },
-});
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+
+const io = new Server(server);
 
 app.use(express.json());
 
+require('./sockets/socketChat')(io);
+
 app.use(express.static(`${__dirname}/public`));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server started on http//:localhost:${PORT}`);
 });
