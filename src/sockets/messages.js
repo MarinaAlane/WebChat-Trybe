a4c15e5e24868ceb3a2a47682c5deace46acd48d
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-12-07 13:47:32 -0300

    FEAT: Create messages socket

--- src/sockets/messages.js
+++ src/sockets/messages.js
@@ -1,3 +1,10 @@
-module.exports = (io) => io.on('connection', () => {
+const getCurrentDate = require('../utils/getCurrentDate');
 
+module.exports = (io) => io.on('connection', (socket) => {
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const date = getCurrentDate('pt-BR');
+    const message = `${date} - ${nickname}: ${chatMessage}`;
+
+    io.emit('message', message);
+  });
 });
