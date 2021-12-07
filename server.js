270899875def004c2e2c25fdeb98e3e5934bf7c0
Bruno Duarte <brunoarduarte@gmail.com>
2021-12-07 20:47:55 -0300

    Requisito 1

--- server.js
+++ server.js
@@ -1,1 +1,43 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+const CORS = require('cors');
+const moment = require('moment');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(CORS());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+  res.render('index.html');
+});
+
+const messages = [];
+
+io.on('connection', (socket) => {
+  socket.emit('previousMessages', messages);
+
+  socket.on('message', (message) => {
+    messages.push(message);
+    const time = moment().format('DD-MM-YYYY hh:mm:ss A');
+    const formatedMessage = `${time} - ${message.nickname}: ${message.chatMessage}`;
+    io.emit('message', formatedMessage);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+module.exports = {
+  io,
+  moment,
+};
