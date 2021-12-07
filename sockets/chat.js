6c1130d1ec25b76d92b82abd7f92ac7c2e5dec77
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-12-07 20:21:49 -0300

    req 2

--- sockets/chat.js
+++ sockets/chat.js
@@ -4,7 +4,6 @@ module.exports = (io) => {
   io.on('connection', (socket) => {
     socket.on('message', ({ chatMessage, nickname }) => {
       const messageTime = getTime();
-
       io.emit('message', `${messageTime} - ${nickname}: ${chatMessage}`);
     });
   });
--- views/js/chat.js
+++ views/js/chat.js
@@ -1,3 +1,54 @@
 const socket = window.io();
 
-console.log(socket);
+// adapted from => https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+const generateRandomId = (nicknameLength) => {
+  let nickname = '';
+  const characters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+  for (let index = 0; index < nicknameLength; index += 1) {
+    nickname += characters.charAt(Math.floor(Math.random() * nicknameLength));
+  }
+
+  return nickname;
+};
+
+let nickname = generateRandomId(16);
+
+const onlineUsersList = document.getElementById('online-users-list');
+const sendMessageBtn = document.getElementById('send-btn');
+const messageInput = document.getElementById('message-input');
+const messagesList = document.getElementById('messages-list');
+const changeNicknameBtn = document.getElementById('nickname-btn');
+const nicknameInput = document.getElementById('nickname-input');
+
+window.onload = () => {
+  const onlineUsers = document.createElement('li');
+  onlineUsers.innerText = nickname;
+  onlineUsers.dataset.testid = 'online-user';
+  onlineUsersList.appendChild(onlineUsers);
+};
+
+changeNicknameBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  nickname = nicknameInput.value;
+  nicknameInput.value = '';
+  return false;
+});
+
+sendMessageBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  socket.emit('message', { 
+    chatMessage: messageInput.value,
+    nickname, 
+  });
+  messageInput.value = '';
+  return false;
+});
+
+const newMessage = (data) => {
+  const li = document.createElement('li');
+  li.innerText = data;
+  li.dataset.testid = 'message';
+  messagesList.appendChild(li);
+};
+
+socket.on('message', (data) => newMessage(data));
