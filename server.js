053dd31f2459993d18c75b5817e2bcc92e7aca76
Filizzola <juliofilizzola@hotmail.com>
2021-12-07 13:02:11 -0300

    BUILD: criando estrutura de dados no serve.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// Faça seu código aqui
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const socketIo = require('socket.io');
+
+app.use('/grupo1', express.static(path.join(__dirname, 'public')));
+app.use('/grupo2', express.static(path.join(__dirname, 'public')));
+
+const serve = app.listen(port, () => console.log(`Example app listening on port %s`, port));
+
+const io = socketIo(serve);
