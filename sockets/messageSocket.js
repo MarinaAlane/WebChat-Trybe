0c39ac59abb817131acbd4f0460ff6791cb13644
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-12-07 18:35:33 -0300

    fix: event names

--- sockets/messageSocket.js
+++ sockets/messageSocket.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.on('sendMessage', ({ chatMessage, nickname }) => {
+    socket.on('message', ({ chatMessage, nickname }) => {
       const date = moment();
       const formatedDate = date.format('DD-MM-yyyy hh:mm:ss A');
 
