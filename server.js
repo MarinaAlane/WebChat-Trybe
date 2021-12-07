ae64b983a6c64676b1d384a9f72dbb7f0bc1dad5
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-12-07 15:43:02 -0300

    feat: add server configurations

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-// Faça seu código aqui
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (_request, response) => response.render('pages/index'));
+
+const http = require('http').createServer(app);
+
+const { PORT = 3000 } = process.env;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`New user connected. ID: ${socket.id}`);
+});
+
+http.listen(PORT, () => console.log(`App runnign on port: ${PORT}`));
