daaa119b3979c5f62e887643778868b56503258c
josanjohnata <josanjohnata@gmail.com>
2021-12-08 20:45:22 -0300

    Adiciona requisot 2

--- src/sockets/messages.js
+++ src/sockets/messages.js
@@ -1,10 +1,22 @@
 const getCurrentDate = require('../utils/getCurrentDate');
+const MessagesModels = require('../database/models/Messages');
 
 module.exports = (io) => io.on('connection', (socket) => {
-  socket.on('message', ({ chatMessage, nickname }) => {
-    const date = getCurrentDate('pt-BR');
-    const message = `${date} - ${nickname}: ${chatMessage}`;
+  socket.on('message', async ({ chatMessage: message, nickname }) => {
+    const timestamp = getCurrentDate('pt-BR');
 
-    io.emit('message', message);
+    await MessagesModels.addNewMessage({ message, nickname, timestamp });
+
+    io.emit('message', `${timestamp} - ${nickname}: ${message}`);
   });
+
+  socket.on('loadMessages', async () => {
+    const allMessages = await MessagesModels.getAllMessages();
+
+    io.emit('loadMessages', allMessages);
+  });
+
+  socket.on('userSignIn', (nickName) => socket.broadcast.emit('userSignIn', nickName));
+
+  socket.on('loggedUsers', (loggedUser) => socket.broadcast.emit('loggedUsers', loggedUser));
 });
