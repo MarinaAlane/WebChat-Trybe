2b4b45c30dedbcfaa4ad269e54a3971926cfb2cc
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-12-08 15:42:29 -0300

    feat: add message to db

--- sockets/messageSocket.js
+++ sockets/messageSocket.js
@@ -1,10 +1,17 @@
 const moment = require('moment');
+const Message = require('../models/Message');
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.on('message', ({ chatMessage, nickname }) => {
+    socket.on('message', async ({ chatMessage, nickname }) => {
       const date = moment();
       const formatedDate = date.format('DD-MM-yyyy hh:mm:ss A');
+
+      await Message.create({
+        message: chatMessage,
+        nickname,
+        timestamp: formatedDate,
+      });
 
       io.emit('message', `${formatedDate} - ${nickname}: ${chatMessage}`);
     });
