02af4d46ee70b50106cc69cc8c50c001534300bb
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-12-08 15:47:57 -0300

    feat: add get all messages

--- models/Message.js
+++ models/Message.js
@@ -7,4 +7,13 @@ module.exports = {
 
     await messagesCollection.insertOne({ ...message });
   },
+
+  getAll: async () => {
+    const db = await connect();
+    const messagesCollection = db.collection('messages');
+
+    const messages = await messagesCollection.find().toArray();
+
+    return messages;
+  },
 };
