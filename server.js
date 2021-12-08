db4ef666782067d437e70465b0883056eed3b7a4
Filizzola <juliofilizzola@hotmail.com>
2021-12-08 18:22:51 -0300

    BUILD: importando express e cors

--- server.js
+++ server.js
@@ -1,24 +1,3 @@
 const express = require('express');
+const cors = require('cors');
 
-const app = express();
-const port = 3000;
-const path = require('path');
-const socketIo = require('socket.io');
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-
-const serve = app.listen(port, () => console.log('Example app listening on port %s', port));
-
-const io = socketIo(serve);
-
-const messages = [];
-
-io.on('connection', (socket) => {
-  console.log('new connection');
-  io.emit('update_messages', messages);
-  socket.on('new_message', (data) => {
-    console.log(data)
-    messages.push(data);
-    io.emit('update_messages', messages);
-  });
-});
