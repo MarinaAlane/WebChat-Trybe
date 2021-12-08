700496f4a0f277ae98ac3b9d5183de0653a00d7c
Daniel <danielfdoficial@gmail.com>
2021-12-08 17:11:37 -0300

    requisito 4

--- sockets/webchat.js
+++ sockets/webchat.js
@@ -3,15 +3,39 @@ const messagesController = require('../controller/messagesController');
 
 const timestamp = moment().format('MM-DD-YYYY h:mm:ss A');
 
+const returnNicknames = ({ io, nicknames }) => io.emit('online-users', nicknames);
+
+let nicknames = [];
+
+const emitNicknames = ({ io, id, nickname, action }) => {
+  if (action === 'newNickname') {
+    nicknames.push({ id, nickname });
+    return returnNicknames({ io, nicknames });
+  }
+  
+  const filteredNicknames = nicknames.filter((user) => user.id !== id);
+  nicknames = filteredNicknames;
+  nicknames.push({ id, nickname });
+  returnNicknames({ io, nicknames });
+};
+
 module.exports = (io) => io.on('connection', (socket) => {
   const { id } = socket;
+
+  socket.on('userNickname', ({ nickname, action }) => emitNicknames({ io, id, nickname, action }));
+
   socket.on('message', async ({ chatMessage: message, nickname }) => {
     console.log(`${timestamp} - ${nickname}: ${message}`);
     await messagesController.createMessage({ message, nickname, timestamp });
     io.emit('message', `${timestamp} - ${nickname}: ${message}`);
   });
 
-  socket.on('generateNickname', () => {
-    socket.emit('generateNickname', id);
+  socket.on('disconnect', () => {
+    const filteredNicknames = nicknames.filter((user) => user.id !== id);
+
+    nicknames = filteredNicknames;
+    returnNicknames({ io, nicknames });
   });
+
+  socket.on('generateNickname', () => socket.emit('generateNickname', id));
 });
--- view/js/webchat.js
+++ view/js/webchat.js
@@ -8,6 +8,11 @@ const inputNickname = document.querySelector('#nicknameInput');
 
 const userNickname = document.querySelector('#nickname');
 
+const sendUserNickNameToSessionStorage = (nickname) => {
+  console.log(nickname);
+  sessionStorage.setItem('nickname', nickname);
+};
+
 formMessage.addEventListener('submit', (e) => {
   e.preventDefault();
   socket.emit('message', { chatMessage: inputMessage.value, nickname: userNickname.innerText });
@@ -18,6 +23,8 @@ formMessage.addEventListener('submit', (e) => {
 formNickname.addEventListener('submit', (e) => {
   e.preventDefault();
   userNickname.innerText = inputNickname.value;
+  sendUserNickNameToSessionStorage(inputNickname.value);
+  socket.emit('userNickname', { nickname: inputNickname.value, action: 'updateNickname' });
   inputNickname.value = '';
 });
 
@@ -35,10 +42,28 @@ const showUserNickname = () => {
   socket.emit('generateNickname');
 
   socket.on('generateNickname', (id) => {
-    userNickname.innerText = cutID(id);
+    const nickname = cutID(id);
+    userNickname.innerText = nickname;
+    sendUserNickNameToSessionStorage(nickname);
+    socket.emit('userNickname', { nickname, action: 'newNickname' });
   });
 };
 
 showUserNickname();
 
+const showOnlineUsers = (nicknames) => {
+  const onlineUsersList = document.querySelector('#users-online');
+  onlineUsersList.innerText = '';
+
+  nicknames.forEach(({ nickname }) => {
+    if (nickname !== sessionStorage.getItem('nickname')) {
+      const li = document.createElement('li');
+      li.setAttribute('data-testid', 'online-user');
+      li.innerText = nickname;
+      onlineUsersList.appendChild(li);
+    }
+  });
+};
+
 socket.on('message', (message) => createMessage(message));
+socket.on('online-users', (nicknames) => showOnlineUsers(nicknames));
