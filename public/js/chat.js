257c29b444e39725d031bfc7562585d596d1709b
Matheus Alexandre <alexandremhm@gmail.com>
2021-12-08 15:36:44 -0300

    Fix lint

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,10 +3,10 @@ const socket = window.io();
 const form = document.querySelector('#message-form');
 const nicknameForm = document.querySelector('#nickname-form');
 const newnickname = document.querySelector('#nicknamebox');
+const dataTest = 'data-testid';
 
 const inputMessage = document.querySelector('#messageInput');
 const usersList = document.querySelector('#users');
-const usersListArr = document.getElementById('users').getElementsByTagName('li');
 
 const makeNickName = () => {
   let text = '';
@@ -56,7 +56,7 @@ const createMessage = (userMessage) => {
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
   li.innerText = userMessage;
-  li.setAttribute('data-testid', 'message');
+  li.setAttribute(dataTest, 'message');
   messagesUl.appendChild(li); 
 };
 
@@ -65,14 +65,12 @@ socket.on('message', (userMessage) => createMessage(userMessage));
 socket.on('update-nicknames', (arrayUsers) => {
   usersList.innerHTML = '';
   const arr = [];
-
-  console.log(arrayUsers)
-
+  
   const usernickname = sessionStorage.getItem('nickname');
 
   const fisrtLi = document.createElement('li');
   fisrtLi.innerText = usernickname;
-  fisrtLi.setAttribute('data-testid', 'online-user');
+  fisrtLi.setAttribute(dataTest, 'online-user');
 
   arr.push(fisrtLi);
   
@@ -80,30 +78,8 @@ socket.on('update-nicknames', (arrayUsers) => {
     if (element.user === usernickname) return;
     const li = document.createElement('li');
     li.innerText = element.user;
-    li.setAttribute('data-testid', 'online-user');
+    li.setAttribute(dataTest, 'online-user');
     arr.push(li);
   });
   arr.forEach((elements) => usersList.appendChild(elements));
 });
-
-// socket.on('disconnect', () => {
-//   const user = sessionStorage.getItem('nickname');
-//   socket.emit('disconect-user', user);
-// });
-
-// window.onbeforeunload = () => {
-//   socket.emit('disconnect', sessionStorage.getItem('nickname')); 
-// };
-
-// window.addEventListener('beforeunload',
-// () => {
-//   // socket.emit('event', { user: sessionStorage.getItem('nickname'), id: socket.id });
-//   const userToRemove = sessionStorage.getItem('nickname');
-//   const newArr = Array.from(usersListArr).map((element) => element.innerText);
-//   if (userToRemove.length > 1) {
-//       console.log(newArr);
-//       const arr = newArr.filter((user) => user !== userToRemove);
-//       socket.emit('event', arr);
-//     }
-//   sessionStorage.clear();
-//   });
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,5 +1,16 @@
 const arrayUsers = [];
 
+const newUserFunc = (io, { user, userOld }, socket) => {
+  const userExists = arrayUsers.find((element) => element.user === userOld);
+    if (userExists) {
+      const index = arrayUsers.indexOf(userExists);
+      arrayUsers[index].user = user;
+    } else {
+      arrayUsers.push({ user, socket });
+    }
+    io.emit('update-nicknames', arrayUsers.map((element) => ({ user: element.user })));
+};
+
 module.exports = (io) => io.on('connection', (socket) => { 
   socket.on('message', ({ chatMessage, nickname }) => {   
     const timeHour = new Date().toLocaleTimeString('pt-br', { hour12: true });
@@ -10,20 +21,11 @@ module.exports = (io) => io.on('connection', (socket) => {
   });
 
   socket.on('new-user', ({ user, userOld }) => {
-    const userExists = arrayUsers.find((element) => element.user === userOld);
-    if (userExists) {
-      const index = arrayUsers.indexOf(userExists);
-      arrayUsers[index].user = user;
-    } else {
-      arrayUsers.push({user, socket});
-    }
-    io.emit('update-nicknames', arrayUsers.map(({user}) => ({ user })));
+    newUserFunc(io, { user, userOld }, socket);
   });
 
 socket.on('disconnect', () => {
- arrayUsers.splice(arrayUsers.indexOf(arrayUsers.find((element)=> element.socket === socket)),1);
-  console.log(arrayUsers)
-  io.emit('update-nicknames', arrayUsers.map(({user}) => ({ user })))
+ arrayUsers.splice(arrayUsers.indexOf(arrayUsers.find((element) => element.socket === socket)), 1);
+  io.emit('update-nicknames', arrayUsers.map(({ user }) => ({ user })));
 });
-
 });
