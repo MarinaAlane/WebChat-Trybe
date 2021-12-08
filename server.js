e79b97a9d1b25986ecdc16f3798c243a52fd46c7
hheloisa <hhackenhaar@gmail.com>
2021-12-08 19:08:00 -0300

    Fix: da poderes http ao server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
+
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
+server.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
