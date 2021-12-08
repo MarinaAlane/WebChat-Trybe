7bf23f7657e2245a3ae97d4b0dbe11b6383e034a
hheloisa <hhackenhaar@gmail.com>
2021-12-08 19:11:18 -0300

    Fix: evolui server, equipado com biblioteca socket.io.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const http = require('http');
-
-const app = express();
-const server = http.createServer(app);
+const { Server } = require('socket.io');
 
 const PORT = 3000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log(`-> ${socket.id} está conectado`);
+});
 
 server.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
