6f287fa0f1cc29f59c554d5d6a5644556c6cf9f8
Leandro Lopes <leandro_carlos@uol.com.br>
2021-12-08 11:41:54 -0300

    requisito 1

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const app = require('express')();
-const http = require('http').createServer(app);
+const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
     cors: {
@@ -12,14 +16,21 @@ const io = require('socket.io')(http, {
 app.use(cors());
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('a user connected', socket.id);
     
     socket.on('disconnect', () => {
-        console.log('user disconnected :(');
+        console.log('user disconnected :(', socket.id);
     });
     
-    socket.on('mensagem', (msg) => {
+    socket.on('message', (msg) => {
         console.log(`alguem mandou uma mensagem: ${msg}`);
+        const { chatMessage, nickname } = msg;
+        const date = new Date();
+        const dateActual = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+        const timeActual = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+        const printDados = `${dateActual} ${timeActual} - ${nickname}: ${chatMessage}`;
+        io.emit('message', printDados);
     });
 });
 
