dcdb17f77284d86a329a5c9ae884c5d19015a8bf
Matheus <matheusaraujo.dev@gmail.com>
2021-12-08 16:48:19 -0300

    Adiciona req 1

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const http = require('http');
+const { format } = require('date-fns');
 
 const app = express();
 const PORT = 3000;
@@ -17,21 +18,30 @@ app.use(cors());
 const socketIoServer = http.createServer(app);
 const io = require('socket.io')(socketIoServer, {
   cors: {
-    origin: `http://localhost:3000${PORT}`, // url aceita pelo cors
+    origin: `http://localhost:${PORT}`, // url aceita pelo cors
     methods: ['GET', 'POST'], // Métodos aceitos pela url
   },
 });
 
-app.get('/board', (_req, res) => {
+io.on('connection', (socket) => {
+  const timestamp = format(new Date(), 'dd-MM-yyyy HH:mm:ss');
+  console.log(`usuário ${socket.id} conectado`);
+
+  socket.on('message', (msg) => {
+    io.emit('message', `${timestamp} - ${msg.nickname}: ${msg.chatMessage}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`usuário ${socket.id} desconectou`);
+  });
+});
+
+app.get('/', (_req, res) => {
   res.render('board');
 });
 
 app.post('/post', async (req, res) => {
   const { chatMessage, nickname } = req.body;
-
-  const message = {
-    nickname, chatMessage,
-  };
 
   const getHour = `${new Date().getHours()}`;
   let hourSign = '';
@@ -39,13 +49,15 @@ app.post('/post', async (req, res) => {
     hourSign = 'AM';
   } hourSign = 'PM';
 
-  const timestamp = await `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`;
+  const timestamp = format(new Date(), 'dd-MM-yyyy HH:mm:ss');
 
-  if (!message) return res.status(422).json({ message: 'Falta enviar sua mensagem' });
+  if (!chatMessage || !nickname) {
+    return res.status(422).json({ message: 'Falta enviar sua mensagem ou nickname' });
+  }
   
   io.emit('message', `${timestamp} ${hourSign} ${nickname} ${chatMessage}`);
 
-  return res.status(200).json(message);
+  return res.status(200).json({ nickname, chatMessage });
 });
 
 socketIoServer.listen(PORT, () => {
