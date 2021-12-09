75d6a5a0a46421b1ca31309a2064d5315a4aac4a
Marcelo <marsleite@gmail.com>
2021-12-08 22:54:11 -0300

    criação de novos sockets, setNickname para atender os usuarios online, req 4.

--- server.js
+++ server.js
@@ -18,16 +18,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const ftMsg = moment().format('MM-DD-YYYY h:mm A');
 
-const users = [];
+let users = [];
 
 io.on('connection', (socket) => {
   console.log(`Socket ${socket.id} connected`);
   const userOnline = socket.id.slice(0, 16);
-  users.push(userOnline);
-  console.log(users);
-  socket.emit('user-connected', userOnline);
   
-  io.emit('userOnline', users);
+  socket.emit('user-connected', userOnline);
+
+  socket.on('setNickname', (nickname) => {
+    users = users.filter((user) => user.id !== socket.id);
+    users.push({ id: socket.id, nickname });
+    io.emit('userOnline', users);
+  });
+
+  socket.on('disconnect', () => {
+    users = users.filter((user) => user.id !== socket.id);
+    io.emit('userOnline', users);
+  });
 
   socket.on('message', (data) => {
     io.emit('message', `${ftMsg} - ${data.nickname}: ${data.chatMessage}`);
