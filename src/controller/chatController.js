18918fec24705b569e4e73c89b4ee31f851b15dc
Filizzola <juliofilizzola@hotmail.com>
2021-12-09 16:39:04 -0300

    BUILD: criando controller

--- src/controller/chatController.js
+++ src/controller/chatController.js
@@ -1,7 +1,25 @@
-const setChatMessage = (data) => data;
-const getChatMessage = (data) => data;
+const router = require('express').Router();
+const {
+  getAllMessages,
+  setMessage,
+} = require('../models/service');
 
-module.exports = {
-  setChatMessage,
-  getChatMessage,
-};
+router.get('/chat', async (_req, res) => {
+  const allMessages = await getAllMessages();
+  return res.status(200).json(allMessages);
+});
+
+router.post('/chat', async (req, res) => {
+  try {
+    const insertOneMessage = await setMessage(req.body);
+    if (!insertOneMessage) {
+      return res.status(404).json({ message: 'erro interno' });
+    }
+    return res.status(200).json(insertOneMessage);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
