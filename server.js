04178857b60af5a08d6ea3eb61764595c811adfa
Silas Gomes <islenegomes@gmail.com>
2021-12-09 16:19:58 -0300

    Requisito 1 finalizado

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const path = require('path');
 
+// a data foi feita através de estudo de como poderia simplificar esse sistema, e encontrei nesse site: https://www.horadecodar.com.br/2021/04/03/como-pegar-a-data-atual-com-javascript/
+const date = new Date();
+const day = String(date.getDate()).padStart(2, '0');
+const month = String(date.getMonth()).padStart(2, '0');
+const year = date.getFullYear();
+const hours = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+const fulldate = `${day}-${month}-${year} ${hours}`;
+
 const app = express();
 const server = require('http').createServer(app);
 const cors = require('cors');
@@ -15,23 +23,24 @@ const io = require('socket.io')(server, {
 
 app.use(cors());
 
-app.use(express.static(path.join('views')));
-app.set('views', path.join('views'));
+// app.use(express.static(path.join('views')));
+// app.set('views', path.join('views'));
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 io.on('connection', (socket) => {
   console.log(`Usuário ${socket.id} conectado`);
 
+  socket.on('message', (msg) => {
+    const { nickname, chatMessage } = msg;
+    io.emit('message', `${fulldate} - ${nickname}: ${chatMessage}`);
+  });
+
   socket.on('disconnect', () => {
     console.log(`Usuário ${socket.id} desconectado`);
   });
-
-  socket.on('chatMessage', (msg) => {
-    console.log(`message: ${socket.id} diz: ${msg}`);
-  });
-});
-
-app.get('/', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 server.listen(PORT, () => console.log('Ouvindo a porta 3000'));
