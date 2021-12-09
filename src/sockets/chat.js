ac1ee3a2517552d02d9835a9d570f3b9ab814d9b
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2021-12-09 13:50:17 -0300

    feat: Requisito 2 e começando a 4

--- src/sockets/chat.js
+++ src/sockets/chat.js
@@ -1,7 +1,17 @@
 const { getTime } = require('../utils/chat');
 
+const users = [];
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
+    const id = socket.id.substring(0, 16);
+
+    users.push(id);
+
+    socket.emit('id', users);
+
+    socket.emit('connectUsers', users);
+
     socket.on('message', ({ chatMessage, nickname }) => {
       const messageTime = getTime();
 
--- src/views/js/chat.js
+++ src/views/js/chat.js
@@ -1,3 +1,53 @@
 const socket = window.io();
 
-console.log(socket);
+const getUsersList = document.getElementById('onlineUsersId');
+const buttonMessage = document.getElementById('sendButton');
+const messageInput = document.getElementById('messageInput');
+const listMessage = document.getElementById('listMessage');
+const saveNickNameButton = document.getElementById('nicknameButton');
+const nickNameInput = document.getElementById('nicknameInput');
+
+const saveRandomId = (id) => {
+  const onlineUsers = document.createElement('li');
+  onlineUsers.innerText = id;
+  onlineUsers.setAttribute('data-testid', 'online-user');
+  getUsersList.appendChild(onlineUsers);
+};
+
+saveNickNameButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  socket.emit('id', {
+    nickname: nickNameInput.value,
+  });
+  return false;
+});
+
+buttonMessage.addEventListener('click', (event) => {
+  event.preventDefault();
+  socket.emit('message', {
+    chatMessage: messageInput.value,
+    nickname: nickNameInput.value,
+  });
+  messageInput.value = '';
+  return false;
+});
+
+const newMessage = (data) => {
+  const li = document.createElement('li');
+  li.innerText = data;
+  li.dataset.testid = 'message';
+  listMessage.appendChild(li);
+};
+
+socket.on('message', (data) => newMessage(data));
+
+window.onload = () => {
+  socket.on('id', (id) => {
+    const removeLastPosition = id[id.length - 1];
+    sessionStorage.setItem('nickname', removeLastPosition);
+    saveRandomId(removeLastPosition);
+      for (let index = 0; index < id.length - 1; index += 1) {
+        const user = id[id[index]];
+      }
+  });
+};
