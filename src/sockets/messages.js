17463382524f76b689a36756ddf27f029081598f
josanjohnata <josanjohnata@gmail.com>
2021-12-08 21:12:22 -0300

    Corrigindo o Lint

--- src/sockets/messages.js
+++ src/sockets/messages.js
@@ -2,10 +2,9 @@ const getCurrentDate = require('../utils/getCurrentDate');
 const MessagesModels = require('../database/models/Messages');
 
 module.exports = (io) => io.on('connection', (socket) => {
-
     socket.on('message', async ({ chatMessage: message, nickname }) => {
         const timestamp = getCurrentDate('pt-BR');
-
+        
         await MessagesModels.addNewMessage({ message, nickname, timestamp });
 
         io.emit('message', `${timestamp} - ${nickname}: ${message}`);
@@ -16,5 +15,4 @@ module.exports = (io) => io.on('connection', (socket) => {
 
         io.emit('loadMessages', allMessages);
     });
-
 });
