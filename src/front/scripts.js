0417f4bee5a7b8ca989716f2672336b03d6b5d24
josanjohnata <josanjohnata@gmail.com>
2021-12-08 21:08:53 -0300

    Corrigindo o Lint

--- src/front/scripts.js
+++ src/front/scripts.js
@@ -5,7 +5,9 @@ const testIds = {
     nickNameButton: '[data-testid="nickname-button"]',
     sendButton: '[data-testid="send-button"]',
 };
+
 const socket = window.io();
+
 function setSessionUser(nickName) {
     sessionStorage.setItem('webChatUser', nickName);
 }
@@ -15,31 +17,40 @@ function randomNickNameUser() {
     setSessionUser(nickName);
     return nickName;
 }
+
 function renderUser(nickName) {
     const onlineUsersUl = document.getElementById('online-user');
     const li = document.createElement('li');
     li.setAttribute('data-testid', 'online-user');
     li.innerText = nickName;
+
     onlineUsersUl.appendChild(li);
 }
+
 function getAllLoggedUsers() {
     const allUsers = [];
+
     document.querySelectorAll(testIds.onlineUser)
         .forEach((child) => allUsers.push(child.innerText));
+
     return allUsers;
 }
+
 function renderNickName() {
     const nickName = randomNickNameUser();
     renderUser(nickName);
     socket.emit('userSignIn', nickName);
 }
+
 function renderMessageList(message) {
     const messagesUl = document.getElementById('messages');
     const li = document.createElement('li');
     li.setAttribute('data-testid', 'message');
     li.innerText = message;
+
     messagesUl.appendChild(li);
 }
+
 function sendNewMessage() {
     const nickname = document.querySelector(testIds.onlineUser).innerText;
     const chatMessage = document.querySelector(testIds.messageBox).value;
@@ -75,22 +86,28 @@ document.querySelector(testIds.sendButton).addEventListener('click', (e) => {
     e.preventDefault();
     sendNewMessage();
 });
+
 socket.on('message', (message) => {
     renderMessageList(message);
 });
+
 socket.on('loadMessages', (messages) => {
     document.getElementById('messages').innerHTML = '';
     messages.forEach(({ message, nickname, timestamp }) => {
         renderMessageList(`${timestamp} - ${nickname}: ${message}`);
     });
 });
+
 socket.on('userSignIn', (nickName) => {
     renderUser(nickName);
     socket.emit('loggedUsers', sessionStorage.getItem('webChatUser'));
 });
+
 socket.on('loggedUsers', (loggedUser) => {
     const allUsers = getAllLoggedUsers();
+
     if (allUsers.includes(loggedUser)) return;
+    
     renderUser(loggedUser);
 });
 
