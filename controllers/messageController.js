625f370d524565bde407313502560075aa304afa
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-12-08 22:11:32 -0300

    Add: Create files to store messages

---
+++ controllers/messageController.js
@@ -0,0 +1,14 @@
+const MessageModel = require('../models/messageModel');
+
+const storeMessage = async (req, res) => {
+
+};
+
+const getMessages = async (req, res) => {
+
+};
+
+module.exports = {
+  storeMessage,
+  getMessages,
+};
---
+++ models/messageModel.js
@@ -0,0 +1,11 @@
+const { ObjectId } = require('mongodb');
+const connection = require('./connection');
+
+const storeMessage = async (/** params */) => {};
+
+const getMessages = async (/** params */) => {};
+
+module.exports = {
+  storeMessage,
+  getMessages,
+};
