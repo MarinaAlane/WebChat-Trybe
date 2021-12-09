d2f585d1199a8b69003bb8b8067659f0200c5373
Jessica Arruda <sicarruda@gmail.com>
2021-12-09 15:21:59 -0300

    exercicio 1

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 // Faça seu código aqui
+require('dotenv').config();
+const express = require('express');
+// https://moment.github.io/luxon/#/tour
+const { DateTime } = require('luxon');
+const path = require('path');
+
+const app = express();
+
+const { Server } = require('socket.io');
+const http = require('http').createServer(app);
+
+const io = new Server(http);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+io.on('connection', (socket) => {
+  console.log(`${socket.id} entrou`);
+  socket.on('message', (msg) => {
+    io.emit('message',
+    `${DateTime.now().toFormat('dd-LL-yyyy hh:mm:ss')} ${msg.nickname}: ${msg.chatMessage}`);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Servidor HTTP ouvindo na porta ${PORT}`);
+});
