6d1adb86b10a412b0328fdeee352a5788ec9bd89
Jessica Arruda <sicarruda@gmail.com>
2021-12-09 17:27:47 -0300

    exercicio 2

--- server.js
+++ server.js
@@ -16,8 +16,21 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+function generateToken(length) {
+  // https://www.ti-enxame.com/pt/javascript/crie-um-token-aleatorio-em-javascript-com-base-nos-detalhes-do-usuario/941136694/
+  const a = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
+  const b = [];  
+  for (let i = 0; i < length; i += 1) {
+      const j = (Math.random() * (a.length - 1)).toFixed(0);
+      b[i] = a[j];
+  }
+  return b.join('');
+}
+
 io.on('connection', (socket) => {
-  console.log(`${socket.id} entrou`);
+  socket.emit('logado', generateToken(16));
+
   socket.on('message', (msg) => {
     io.emit('message',
     `${DateTime.now().toFormat('dd-LL-yyyy hh:mm:ss')} ${msg.nickname}: ${msg.chatMessage}`);
