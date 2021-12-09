a33a5ba09083c695f069cf02b5a84adce9e9fda5
heyset <c.inaciomatheus@gmail.com>
2021-12-09 17:08:26 -0300

    Adiciona eventos de lista de clientes

--- services/chat.js
+++ services/chat.js
@@ -7,21 +7,53 @@ const getParsedMessage = ({ chatMessage, nickname, timestamp }) => {
   return message;
 };
 
+let connectedClients = [];
+
+const emitClientList = (io) => io.emit('client-list', connectedClients);
+
+const handleMessageEvent = ({ chatMessage, nickname, io }) => {
+  const now = new Date();
+  MessageController.createNewMessage({ chatMessage, nickname, timestamp: now.toISOString() });
+  const message = getParsedMessage({ chatMessage, nickname, timestamp: now });
+  io.emit('message', message);
+};
+
+const handleNewClientEvent = ({ nickname, socket, io }) => {
+  connectedClients.push(nickname);
+  emitClientList(io);
+  console.log(connectedClients);
+  Object.defineProperty(socket, 'nickname', { value: nickname, writable: true });
+};
+
+const handleChangeNicknameEvent = ({ newNickname, socket, io }) => {
+  connectedClients = connectedClients.map((nickname) => 
+    (nickname === socket.nickname ? newNickname : nickname));
+  Object.defineProperty(socket, 'nickname', { value: newNickname, writable: true });
+  emitClientList(io);
+};
+
+const handleDisconnectEvent = ({ socket, io }) => {
+  console.log('disconnected');
+  connectedClients = connectedClients.filter((nickname) => nickname !== socket.nickname);
+  console.log(socket.nickname);
+  emitClientList(io);
+};
+
 const connect = (io) => {
   io.on('connection', (socket) => {
     socket.emit('ping');
-    console.log('cliente conectado');
 
     socket.on('pong', () => {
       console.log('pong recebido');
     });
 
-    socket.on('message', ({ chatMessage, nickname }) => {
-      const now = new Date();
-      MessageController.createNewMessage({ chatMessage, nickname, timestamp: now.toISOString() });
-      const message = getParsedMessage({ chatMessage, nickname, timestamp: now });
-      io.emit('message', message);
-    });
+    socket.on('message', (event) => handleMessageEvent({ ...event, io }));
+
+    socket.on('new-client', (event) => handleNewClientEvent({ ...event, socket, io }));
+
+    socket.on('change-nickname', (event) => handleChangeNicknameEvent({ ...event, socket, io }));
+
+    socket.on('disconnect', (event) => handleDisconnectEvent({ ...event, socket, io }));
   });
 };
 
