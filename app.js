29d7325c3c1bdbfad526e1b20dd006f47136e4a8
heyset <c.inaciomatheus@gmail.com>
2021-12-09 14:52:10 -0300

    Views e partials

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/ping', (req, res) => {
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views', 'pages'));
+
+app.get('/', (_req, res) => {
+  res.status(200).render('home');
+});
+
+app.get('/ping', (_req, res) => {
   res.status(200).json({ message: 'ping' });
 });
 
