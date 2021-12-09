c93f580bec43d9aeee294d990a14cf24a8cbf28a
rodolforezende <rodolforrcont@gmail.com>
2021-12-09 09:34:17 -0300

    Fix: Requisito 1

--- public/script/chat.js
+++ public/script/chat.js
@@ -1,13 +1,24 @@
 const socket = window.io('http://localhost:3000');
 
-let nickname = null;
-
 const divUser = document.querySelector('#users');
-socket.on('connection', (id) => {
-  nickname = id;
+
+const createLi = (id) => {
   const liUsers = document.createElement('li');
   liUsers.innerText = id;
   divUser.appendChild(liUsers);
+};
+
+socket.on('connection', (id) => {
+  createLi(id);
+});
+
+socket.on('connection_users', (arrId) => {
+  const lastPosition = arrId[arrId.length - 1];
+  sessionStorage.setItem('nickname', lastPosition);
+  createLi(lastPosition);
+  for (let i = 0; i < arrId.length - 1; i += 1) {
+    createLi(arrId[i]);
+  }
 });
 
 const divMessages = document.querySelector('#messages');
@@ -16,7 +27,7 @@ const formMessage = document.querySelector('#message_form');
 formMessage.addEventListener('submit', (e) => {
   e.preventDefault();
   const chatMessage = inputMessage.value;
-  socket.emit('message', { chatMessage, nickname });
+  socket.emit('message', { chatMessage, nickname: sessionStorage.nickname });
 });
 
 socket.on('message', (inform) => {
--- socket/Chat.js
+++ socket/Chat.js
@@ -1,10 +1,14 @@
 const moment = require('moment');
 
+const arrId = [];
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log('new connection');
     const id = socket.id.substring(0, 16);
-    io.emit('connection', id);
+    arrId.push(id);
+    socket.broadcast.emit('connection', id);
+    socket.emit('connection_users', arrId);
 
     socket.on('message', ({ chatMessage, nickname }) => {
       const date = moment(new Date()).format('DD-MM-yyyy h:mm:ss A');
