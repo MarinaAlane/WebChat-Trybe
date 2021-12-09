27f2e2c856e8e0bf417988045146976018f4ba3d
cristian <cristian.brum2@yahoo.com.br>
2021-12-09 08:49:53 -0300

    [Cristian Brum] Project Webchat

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (_req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log(`usuario ${socket.id}`);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
