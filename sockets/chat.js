9eb9fc20bdbb8e6294031126429ee98762c5cb34
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-12-09 17:00:59 -0300

    Req04 finalmente realizado - lint ok - obg john

--- sockets/chat.js
+++ sockets/chat.js
@@ -5,25 +5,27 @@ const date = dateInform();
 
 const connectedUsers = {};
 
+function handleMessage(message) {
+  if (message.date) {
+    return `${message.date} - ${message.nickname}: ${message.chatMessage}`;
+  }
+  return `${date} - ${message.nickname}: ${message.chatMessage}`;
+}
+
 module.exports = (io) =>
   io.on('connection', (socket) => {
     socket.on('message', async (message) => {
       await saveMessage(message.chatMessage, message.nickname, date);
-      if (message.date) {
-        return io.emit(
-          'message', `${message.date} - ${message.nickname}: ${message.chatMessage}`,
-);
-      }
-      return io.emit(
-        'message', `${date} - ${message.nickname}: ${message.chatMessage}`,
-);
+      return io.emit('message', handleMessage(message));
     });
     socket.on('join', (nickname) => {
       socket.emit('yourId', socket.id);
-      connectedUsers[socket.id] = nickname; io.emit('join', connectedUsers);
+      connectedUsers[socket.id] = nickname;
+      io.emit('join', connectedUsers);
     });
     socket.on('disconnect', () => {
-      delete connectedUsers[socket.id]; io.emit('disconnectedUser', socket.id);
+      delete connectedUsers[socket.id];
+      io.emit('disconnectedUser', socket.id);
     });
     socket.on('changeNickname', (oldNickname, newNickname) => {
       connectedUsers[socket.id] = newNickname;
