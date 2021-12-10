11271b4e82d5dee15268ce396d9c5d2279d035c9
Erick-Marinho <erickgaves@gmail.com>
2021-12-10 16:57:10 -0300

    feat(server): implementando server
    Arquivo contendo configuraçoes do servidor e suas funcionalidades

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
 // Faça seu código aqui
+const express = require('express');
+const path = require('path');
+const socketIO = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+// criando um servidor a partir de http
+const server = require('http').createServer(app);
+// Configurando io já com cors habilitado
+const io = socketIO(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
+
+const publicPath = path.join(__dirname, '/public');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(publicPath));
+app.use(cors);
+
+require('./sockets/chat')(io);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+console.log('-->', publicPath);
