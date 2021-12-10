ad106f61980c8348f53c230ee7a52f918730fc6f
Leandro Lopes <leandro_carlos@uol.com.br>
2021-12-10 09:29:30 -0300

    nova tentativa4

--- date.js
+++ date.js
@@ -1,8 +1,8 @@
 const getDate = (nickname, chatMessage) => {
 const date = new Date();
-const dateActual = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
-const timeActual = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-const printDados = `${dateActual} ${timeActual} - ${nickname}: ${chatMessage}`;
+const dataAtual = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+const horaAtual = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+const printDados = `${dataAtual} ${horaAtual} - ${nickname}: ${chatMessage}`;
 return printDados;
 };
 
