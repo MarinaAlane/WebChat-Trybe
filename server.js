3c8d7a448908a0cd5e1aa9ec43077fb43eb531d4
Felipe Buso <felipe_buso@hotmail.com>
2021-12-10 10:33:55 -0300

    feat: online users requisito 4

--- server.js
+++ server.js
@@ -13,31 +13,48 @@ const PORT = process.env.PORT || 3000;
 
 const messageService = require('./services/messageService');
 
+let usersOnline = [];
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+const newNames = (newNickname) => {
+  const removedUser = usersOnline
+  .filter((user) => user.userId !== newNickname.userId);
+  removedUser.push(newNickname);
+  usersOnline = removedUser;
+};
+
 io.on('connection', (socket) => {
-  // console.log(`${socket.id} conectou`);
-  socket.emit('randomUser', socket.id);
-  
+  socket.emit('connection', usersOnline);
+  socket.on('addUsers', (newUserArray) => { usersOnline = newUserArray; });
+
   socket.on('message', async (objMessage) => {
     const getFormatedMessage = await messageService.createMessage(objMessage);
     io.emit('message', getFormatedMessage);
   });
 
+  socket.on('newUser', (newUser) => socket.broadcast.emit('newUser', newUser));
+
+  socket.on('changeUser', (newNickname) => {
+    newNames(newNickname);    
+    socket.broadcast.emit('changeUser', newNickname);
+  });
+  
   socket.on('disconnect', () => {
-    console.log(`${socket.id} desconectou`);
-  });
-
-  socket.on('onlineUser', (userNickname) => {
-    console.log({ userNickname });
-    socket.emit('onlineUser', userNickname);
-  });
-
-  socket.on('changeUserNickname', (objNickname) => {
-    socket.emit('changeUserNickname', objNickname);
+    const key = socket.id;
+    const removedUser = usersOnline.filter((remove) => remove.userId !== key);
+    usersOnline = removedUser;
+    socket.broadcast.emit('offUser', key);
   });
 });
+
+// io.on('disconnect', (socket) => {
+//   const key = socket.id;
+//   const removedUser = usersOnline.filter((remove) => remove.userId !== key);
+//   usersOnline = removedUser;
+//   socket.broadcast.emit('offUser', key);
+// });
 
 server.listen(PORT, console.log(`Escutando a porta ${PORT}`));
