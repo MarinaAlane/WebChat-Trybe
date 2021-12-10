d9891ceacf7162e5439813d4b218c7f3b30d211c
Thyago Pessoa <flash_thyago@hotmail.com>
2021-12-09 22:36:41 -0300

    Req 4

--- public/script.js
+++ public/script.js
@@ -1,17 +1,22 @@
 const socket = window.io('http://localhost:3000');
 
+const online = 'online-user';
+
+const usersUl = document.getElementById('users');
 const messagesUl = document.getElementById('messages');
 
-const nickNameInput = document.getElementById('nickname');
 const btnSave = document.getElementById('btnSave');
 
 const messageInput = document.getElementById('message');
 const btnMessage = document.getElementById('btnMessage');
 
 btnSave.addEventListener('click', (e) => {
+  const nickNameInput = document.getElementById('nickname');
+  const usersList = document.querySelector('.online-user');
   e.preventDefault();
   
-  socket.emit('changeFirstId', nickNameInput.value);
+  usersList.innerText = nickNameInput.value;
+  socket.emit('changeFirstId', nickNameInput.value, sessionStorage.nickname);
   sessionStorage.setItem('nickname', nickNameInput.value);
 
   nickNameInput.value = '';
@@ -24,13 +29,20 @@ btnMessage.addEventListener('click', (e) => {
   messageInput.value = '';
 });
 
-const createMessage = (greeting) => {
+// const createMessage = (greeting) => {
+//   const createLi = document.createElement('li');
+//   createLi.innerText = greeting;
+//   messagesUl.appendChild(createLi);
+//   return false;
+// };
+
+const createUser = (id) => {
   const createLi = document.createElement('li');
-  createLi.innerText = greeting;
+  createLi.innerText = id;
   const dataTest = 'data-testid';
-  createLi.setAttribute(dataTest, 'online-user');
-  messagesUl.appendChild(createLi);
-  // createLi.setAttribute(data-testid, 'online-user');
+  createLi.setAttribute(dataTest, online);
+  createLi.setAttribute('class', online);
+  usersUl.appendChild(createLi);
   return false;
 };
 
@@ -43,6 +55,29 @@ const createChatMessage = (message) => {
   return false;
 };
 
-socket.on('firstId', (id) => sessionStorage.setItem('nickname', id));
-socket.on('greeting', (message) => createMessage(message));
+socket.on('firstId', (usersArr) => {
+  // Função feita com a ajuda de Tales Coelho.
+  usersUl.innerHTML = '';
+  const id = usersArr[usersArr.length - 1];
+  sessionStorage.setItem('nickname', id);
+  createUser(id);
+  for (let i = 0; i < usersArr.length - 1; i += 1) {
+    createUser(usersArr[i]);
+  }
+});
+
+socket.on('online', (id) => createUser(id));
+socket.on('changeId', (id, oldId) => {
+  const usersList = document.querySelectorAll('.online-user');
+  usersList.forEach((e) => {
+    if (e.innerText === oldId) {
+      e.innerText = id;
+    }
+  });
+});
+
+// socket.on('greeting', (message) => createMessage(message));
 socket.on('message', (message) => createChatMessage(message));
+socket.on('offline', (nick) => { 
+  createChatMessage(nick); 
+});
--- server.js
+++ server.js
@@ -19,30 +19,41 @@ const io = require('socket.io')(http, {
 app.use(cors());
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+let usersOnline = [];
+
 io.on('connection', (socket) => {
   let nick = socket.id.substring(0, 16);
 
-  socket.emit('firstId', nick);
+  usersOnline.push(nick);
 
-  socket.on('changeFirstId', (id) => {
-    nick = id; io.emit('firstId', nick);
+  socket.emit('firstId', usersOnline);
+
+  socket.broadcast.emit('online', nick);
+
+  socket.on('changeFirstId', (id, oldId) => {
+    nick = id;
+    usersOnline = usersOnline.map((e) => (e === oldId ? id : e));
+    socket.broadcast.emit('changeId', nick, oldId);
   });
 
-  socket.emit('greeting', nick);
+  // socket.emit('greeting', nick);
 
-  socket.on('nickname', (user) => {
-    socket.emit('greeting', `Bem-vindo ${user}`);
-    socket.broadcast.emit('greeting', `Usuário conectado. Name: ${user}`);
-  });
+  // socket.on('nickname', (user) => {
+  //   socket.emit('greeting', `Bem-vindo ${user}`);
+  //   socket.broadcast.emit('greeting', `Usuário conectado. Name: ${user}`);
+  // });
 
   socket.on('message', ({ nickname, chatMessage }) => {
     const timestamp = moment(new Date()).format('DD-MM-yyyy h:mm:ss A'); // Problem solved using src: https://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
     io.emit('message', `${timestamp} - ${nickname}: ${chatMessage}`);
   });
 
-  // socket.on('disconnect', () => {
-  //   socket.emit('disconnect', `Usuário ${nick} se desconectou!`);
-  // });
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('offline', `${nick} se desconectou`);
+    usersOnline = usersOnline.filter((id) => (id !== nick));
+    io.emit('firstId', usersOnline);
+    // io.emit('disconnect_user', `Usuário ${id.nick} se desconectou!`);
+  });
 });
 
 app.get('/', (_req, res) => {
