9080e09ba174fbfc94dab93cad74a276bd89e129
Ivaldo Souza <ivaldo.souza@datapar.com.br>
2021-12-10 07:45:28 -0300

    Iniciando o projeto

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Conectado');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
