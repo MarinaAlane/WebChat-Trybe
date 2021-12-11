2a9cd605b88f2dc3391bfc7bb0716f55e91408ad
Erick-Marinho <erickgaves@gmail.com>
2021-12-11 14:00:56 -0300

    feat(js/public): implementando funcionalidades para mensagens, saida e ingresso de clientes

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 const socket = window.io();
 
-socket.on('connect', () => {
-  console.log('Cliente conectado ao servidor');
+const form = document.getElementById('form');
+const messageBox = document.getElementById('message-box');
+// const nickname = 'Admin';
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  socket.emit('message', {
+    nickname: 'Admin',
+    chatMessage: messageBox.value,
+  });
+  messageBox.value = '';
 });
+
+const newMessage = (message) => {
+  console.log(
+    `${message.createdAt} - ${message.nickname}: ${message.chatMessage}`,
+  );
+};
+
+socket.on('message', newMessage);
+socket.on('joined', (msg) => console.log(msg));
+socket.on('endConnection', (msg) => console.log(msg));
