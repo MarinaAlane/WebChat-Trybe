5e07260fb4fc945e97539253f2059d93064fc39e
Erick-Marinho <erickgaves@gmail.com>
2021-12-11 13:56:04 -0300

    ci(server): refatorando arquivo server

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 // Faça seu código aqui
 const express = require('express');
-const path = require('path');
 const socketIO = require('socket.io');
-const cors = require('cors');
 
 const app = express();
-// criando um servidor a partir de http
-const server = require('http').createServer(app);
-// Configurando io já com cors habilitado
+const http = require('http');
+
+const server = http.createServer(app);
+const path = require('path');
+const cors = require('cors');
+
 const io = socketIO(server, {
   cors: {
     origin: 'http://localhost:3000',
   },
 });
 
-const publicPath = path.join(__dirname, '/public');
+const HTML_PUBLIC = path.resolve(__dirname, 'public', 'index.html');
+const PATH_PUBLIC = path.resolve(__dirname, 'public');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(publicPath));
-app.use(cors);
+app.use(express.static(PATH_PUBLIC));
+app.use(cors());
 
 require('./sockets/chat')(io);
+
+app.get('/', (_req, res) => {
+  res.sendFile(HTML_PUBLIC);
+});
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}`));
-
-console.log('-->', publicPath);
