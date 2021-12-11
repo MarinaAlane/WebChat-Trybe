db5564474506e7d5d6c814345b13172dc4fc418e
Erick-Marinho <erickgaves@gmail.com>
2021-12-11 20:23:06 -0300

    excluindo arquivo

--- models/chatModels.js
+++
@@ -1,8 +0,0 @@
-const connection = require('./connection');
-
-const newMessageModel = async (message) => {
-  const db = await connection();
-  await db.collection('messages').insertOne(message);
-};
-
-module.exports = { newMessageModel };
