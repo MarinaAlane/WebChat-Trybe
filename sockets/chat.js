63597b6000fef251a15512b5dc3fe4841e24c244
Erick-Marinho <erickgaves@gmail.com>
2021-12-11 14:02:54 -0300

    feat(chat.js): inserindo funcionalidades para lidar com mensagens no servidor

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,5 +1,30 @@
+const chatModels = require('../models/chatModels');
+const { generateMessage } = require('../utils/message');
+
+const joinedChat = (socket) => {
+  socket.broadcast.emit('joined', `Cliente ${socket.id} ingressou no chat.`);
+};
+
+const newMessage = (io, socket) => {
+  socket.on('message', async (message) => {
+    io.emit('message', generateMessage(message));
+    await chatModels.newMessageModel({ message });
+  });
+};
+
+const outChat = (socket) => {
+  socket.on('disconnect', () => {
+    socket.broadcast.emit(
+      'endConnection',
+      `Cliente ${socket.id} saiu do chat.`,
+    );
+  });
+};
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.log(`Cliente ${socket.id} conectado`);
+    joinedChat(socket);
+    newMessage(io, socket);
+    outChat(socket);
   });
 };
