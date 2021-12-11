a7ea60ca2143d5a66be1d7e309550ef03720b78e
JeanEstevesOSF <jean.esteves2001@gmail.com>
2021-12-11 12:45:26 -0300

    add: requisito 2 (frontend) and fix directory error

---
+++ public/scripts/chat.js
@@ -0,0 +1,59 @@
+const socket = window.io('http://localhost:3000');
+
+const divUser = document.querySelector('#users');
+const divMessages = document.querySelector('#messages');
+const inputMessage = document.querySelector('#message');
+const formMessage = document.querySelector('#message_form');
+const formUser = document.querySelector('#user_form');
+
+const createLi = (id) => {
+  const liUsers = document.createElement('li');
+  liUsers.innerText = id;
+  liUsers.setAttribute('data-testid', 'online-user');
+  liUsers.setAttribute('class', 'online-user');
+  divUser.appendChild(liUsers);
+};
+
+socket.on('connection', (id) => {
+  createLi(id);
+});
+socket.on('connection_users', (arrId) => {
+  const lastPosition = arrId[arrId.length - 1];
+  sessionStorage.setItem('nickname', lastPosition);
+  createLi(lastPosition);
+  for (let i = 0; i < arrId.length - 1; i += 1) {
+    createLi(arrId[i]);
+  }
+});
+
+formMessage.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const chatMessage = inputMessage.value;
+  socket.emit('message', { chatMessage, nickname: sessionStorage.nickname });
+});
+socket.on('message', (inform) => {
+  const liMessage = document.createElement('li');
+  liMessage.innerText = inform;
+  liMessage.setAttribute('data-testid', 'message');
+  divMessages.appendChild(liMessage);
+});
+
+formUser.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const inputUser = document.querySelector('#user');
+  const userNickname = inputUser.value;
+  const username = document.querySelector('.online-user');
+  username.innerText = userNickname;
+  socket.emit('alterUsername', { oldNickname: sessionStorage.nickname, userNickname });
+  sessionStorage.setItem('nickname', userNickname);
+});
+
+socket.on('updateUsers', (oldNickname, userNickname) => {
+  console.log(oldNickname, userNickname);
+  const usersnames = document.querySelectorAll('.online-user');
+  usersnames.forEach((res) => {
+    if (res.innerText === oldNickname) {
+      res.innerText = userNickname;
+    }
+  });
+});
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '/template/default')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, '/index.html'));
