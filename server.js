5cb56ae2782f4b8e9d6a4124e9c1233ea6545ee7
Sergio Martins <sergiomos2010@gmail.com>
2021-12-11 17:39:47 -0300

    create server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Faça seu código aqu
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'], 
+} });
+
+app.use(express.json());
+app.use(cors);
+
+http.listen(PORT, () => {
+  console.log(`Ouvindo na porta ${PORT}`);
+});
