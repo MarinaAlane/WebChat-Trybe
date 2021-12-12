88a536542abf9f73d4b9084aeed17d9aac341344
Sergio Martins <sergiomos2010@gmail.com>
2021-12-11 21:33:25 -0300

    feat: chat css

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-// Faça seu código aqui
+const PORT = process.env.PORT || 3000;
+
+const socketIO = require('socket.io');
+const http = require('http');
+
+const app = require('./app');
+
+const server = http.createServer(app);
+
+const io = socketIO(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST'], 
+} });
+
+server.listen(PORT, () => {
+  console.log(`Ouvindo na porta ${PORT}`);
+});
