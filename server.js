90839626cb1c0ae38e995995c9650ec15512e6cd
IgorMendez <igor.emp@hotmail.com>
2021-12-13 20:59:20 -0300

    Req 02

--- server.js
+++ server.js
@@ -4,27 +4,16 @@ const app = require('express')();
 const path = require('path');
 const http = require('http');
 const { Server } = require('socket.io');
-const { newDate } = require('./src/structures/structures');
 
 const server = http.createServer(app);
 const io = new Server(server); 
 
 const PORT = 3000;
 
+require('./src/socket/socketChat')(io); 
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './src/index.html'));
-});
-
-io.on('connection', (socket) => {
-  console.log(`usuário ${socket.id} conectou`);
-
-  socket.on('message', ({ chatMessage, nickname }) => {
-    io.emit('message', `${newDate()} - ${nickname}: ${chatMessage}`);
-  });
-
-  socket.on('disconnect', () => {
-    console.log(`usuário ${socket.id} desconectou`);
-  });
 });
 
 server.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
--- src/structures/structures.js
+++ src/structures/structures.js
@@ -1,6 +1,7 @@
+const moment = require('moment');
+
 const newDate = () => {
-  const hour = new Date().toLocaleTimeString();
-  const date = new Date().toLocaleDateString('pt-BR');
-  return `${date.replace(/\//g, '-')} ${hour}`;
+  const date2 = moment().format('DD-MM-YYYY hh:mm:ss A');
+  return date2;
 }; 
 module.exports = { newDate };
