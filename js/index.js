257a0c1d7cbd7451a567978409701a4e3a9884e9
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-14 18:48:48 -0300

    estrutura req. 4

--- js/index.js
+++ js/index.js
@@ -6,24 +6,30 @@ const nicknameInput = document.getElementById('nicknameInput');
 const nicknameSubmitButton = document.getElementById('nickButton');
 const onlineUser = document.getElementById('onlineUser');
 
-const userIdPone = Math.ceil((Math.random() * 9).toFixed(2));
-const userIdPtwo = Math.ceil((Math.random() * 2).toFixed(2));
-const connectUserName = `CoCoNuTStRaWOn${userIdPone}${userIdPtwo}`;
 const datatest = 'data-testid';
 
 nicknameSubmitButton.addEventListener('click', () => {
-  sessionStorage.setItem(socket.id, nicknameInput.value);
+  sessionStorage.setItem((socket.id).substring(0, 16), nicknameInput.value);
 
-  onlineUser.innerText = sessionStorage.getItem(socket.id);
+  onlineUser.innerText = sessionStorage.getItem((socket.id).substring(0, 16));
 
-  socket.emit('onLineUsers', sessionStorage.getItem(socket.id));
+  socket.emit('nickname', { socketId: (socket.id).substring(0, 16),
+    nickname: sessionStorage.getItem((socket.id).substring(0, 16)) });
+
+  const usernicks = document.getElementsByClassName('online');
+  for (let i = 0; i < usernicks.length; i += 1) {
+    if (usernicks[i].innerText === (socket.id).substring(0, 16)) {
+      usernicks[i].innerText = nicknameInput.value;
+    }
+  }
 });
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   socket.emit('message', { chatMessage: inputMessage.value, 
-    nickname: sessionStorage.getItem(socket.id) || connectUserName });
+    nickname: sessionStorage
+      .getItem((socket.id).substring(0, 16)) || (socket.id).substring(0, 16) });
 
   inputMessage.value = '';
   return false;
@@ -37,30 +43,49 @@ const createMessage = (message) => {
   messagesUl.appendChild(messagesLi);
 };
 
-const createUsersBox = (users) => {
+const createUsersBox = (user) => {
   const usersUl = document.getElementById('usersBox');
-  usersUl.innerHTML = '';
-  users.forEach((user) => {
-    const li = document.createElement('li');
-    li.setAttribute(datatest, 'online-user');
-    usersUl.appendChild(li);
-    li.innerText = user;
-  });
+  const li = document.createElement('li');
+  li.setAttribute(datatest, 'online-user');
+  li.setAttribute('class', 'online');
+  usersUl.appendChild(li);
+  li.innerText = user;
 };
 
 socket.on('messageServer', (message) => {
-  socket.emit('onLineUsers', socket.id);
+  onlineUser.innerText = (socket.id).substring(0, 16);
+
   createMessage(message);
 });
 
-socket.on('messageServer', () => {
-  onlineUser.innerText = connectUserName;
+socket.on('onLineUsers', (usersId) => {
+  const lastPosition = usersId[usersId.length - 1];
+  createUsersBox(lastPosition);
+  for (let i = 0; i < usersId.length; i += 1) {
+    if (usersId[i] !== usersId[usersId.length - 1]) {
+      createUsersBox(usersId[i]);
+    }
+  }
 });
 
 socket.on('message', (message) => createMessage(message));
 
-socket.on('onLineUsers', (onLineUsers) => {
-  createUsersBox(onLineUsers);
+socket.on('connection', (onLineUser) => createUsersBox(onLineUser));
+
+socket.on('users', (id) => {
+  const usernicks = document.getElementsByClassName('online');
+  for (let i = 0; i < usernicks.length; i += 1) {
+    if (usernicks[i].innerText === id) return usernicks[i].remove();
+  }
+});
+
+socket.on('usersUpdate', (socketId, nickname) => {
+  const usernicks = document.getElementsByClassName('online');
+  for (let i = 0; i < usernicks.length; i += 1) {
+    if (usernicks[i].innerText === socketId) {
+      usernicks[i].innerText = nickname;
+    }
+  }
 });
 
 window.onbeforeunload = () => {
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,26 +1,33 @@
 const utils = require('../utils/functions');
 
-const onlineUsers = [];
+let onlineUsers = [];
 
-module.exports = (io) => io.on('connection', (socket) => {
-  socket.emit('messageServer', 'Coconut Straw: Converse sem grampos - Tecnologia Socket.io.');
-
+const sendMessage = (socket, io) => {
   socket.on('message', ({ chatMessage, nickname }) => {
     const date = utils.dateGenerator();
     io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
   });
-  
-  socket.on('onLineUsers', (user) => {
-    onlineUsers.push(user);
-    console.log(onlineUsers);
-    io.emit('onLineUsers', onlineUsers);
-  });
+};
 
+module.exports = (io) => io.on('connection', (socket) => {
+  socket.emit('messageServer', 'Coconut Straw: Converse sem grampos - Tecnologia Socket.io.');
+
+  sendMessage(socket, io);
+
+  let userId = (socket.id).substring(0, 16);
+  onlineUsers.push(userId);
+
+  socket.broadcast.emit('connection', userId);
+  socket.emit('onLineUsers', onlineUsers);
+
+  socket.on('nickname', ({ socketId, nickname }) => {
+    userId = nickname;
+    onlineUsers = onlineUsers.map((nick) => (nick === socketId ? nickname : nick));
+    socket.broadcast.emit('usersUpdate', socketId, nickname);
+  });
+  
   socket.on('disconnect', () => {
-    const index = onlineUsers.indexOf(socket.id);
-    if (index > -1) {
-      onlineUsers.splice(index, 1);
-    }
-    io.emit('onLineUsers', onlineUsers);
+    onlineUsers = onlineUsers.filter((nick) => nick !== userId);
+    socket.broadcast.emit('users', userId);
   });
 });
