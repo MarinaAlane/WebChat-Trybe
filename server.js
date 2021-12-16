c19b62917942b4ee970e0c9d487c87cccfd3e8e2
Victor Santiago da Cunha <60020689+visantiago99@users.noreply.github.com>
2021-12-16 11:22:25 -0300

    Req 1

--- server.js
+++ server.js
@@ -1,26 +1,44 @@
 const express = require('express');
-const path = require('path');
-const socketIo = require('socket.io');
+// const path = require('path');
+const moment = require('moment');
+const cors = require('cors');
 
 const app = express();
+const http = require('http').createServer(app);
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.json());
 
-const port = 3000;
+const users = [];
 
-const server = app.listen(port, () => console.log(`Listening port ${port}`));
-
-const messages = [];
-// const user = [];
-
-const io = socketIo(server);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*',
+    method: ['GET', 'POST'],
+  },
+});
 
 io.on('connection', (socket) => {
   console.log('new connection');
-  socket.emit('update_messages', messages);
-
-  socket.on('new_message', (data) => {
-    messages.push(data);
-    socket.emit('update_messages', messages);
+  const idUser = socket.id.substring(0, 16);
+  users.push(idUser);
+  socket.broadcast.emit('id_User', idUser);
+  socket.emit('connect_user', users);
+  
+  socket.on('message', ({ chatMessage, nickname }) => {
+    console.log(chatMessage, nickname);
+    const date = moment(new Date()).format('DD-MM-YYYY, h:mm:ss');
+    io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
   });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+const port = 3000;
+
+http.listen(3000, () => {
+  console.log(`listen port ${port}`);
 });
