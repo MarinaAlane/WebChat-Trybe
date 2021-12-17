54af3693d7604e58f49fdc9ccefae348ce99df08
Víctor Faria <vitilevi@hotmail.com>
2021-12-16 22:12:12 -0300

    Adiciona primeiro requisito

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+// app.set('view-engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', './index.html')));
+
+io.on('connection', (socket) => {
+  console.log(`${socket} conectado`);
+
+  socket.on('message', ({ nickname, chatMessage }) => {
+    const date = new Date();
+    const hour = date.toLocaleTimeString();
+    const dateFormated = date.toISOString().split('T')[0].split('-').reverse().join('-');
+    const completeDate = `${dateFormated} ${hour}`;
+    io.emit('message', `${completeDate} - ${nickname}: ${chatMessage}`);
+  });
+});
+
+http.listen(port, () => console.log(`listening on port ${port}!`));
