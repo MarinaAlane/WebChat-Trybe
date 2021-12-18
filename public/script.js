7a0a78d453209b26d5dc29247a537b7841725516
Paulo Henrique Assis <pieit@live.com>
2021-12-18 18:36:49 -0300

    Fixed max line arrow function

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // @ts-nocheck
-/* eslint-disable max-lines-per-function */
+
 const socket = window.io();
 const dataTestid = 'data-testid';
 const messageInput = document.querySelector('#messageInput');
@@ -69,7 +69,6 @@ const newUserAnnouncement = (nickname) => {
   const div2 = document.createElement('div');
   div2.classList.add('message', 'other-message');
   const span = document.createElement('span');
-  // span.setAttribute(dataTestid, 'online-user');
   span.innerText = nickname;
   const span2 = document.createElement('span');
   span2.innerText = ' has joined the chat';
@@ -82,31 +81,16 @@ const newUserAnnouncement = (nickname) => {
 nameButton.addEventListener('click', setName);
 
 const refreshUserList = ({ nickname }) => {
-  const randomNumber = Math.floor(Math.random() * 8) + 1;
-  const src = `https://bootdey.com/img/Content/avatar/avatar${randomNumber}.png`;
   const li = document.createElement('li');
   li.classList.add('clearfix');
-  const img = document.createElement('img');
-  img.setAttribute('src', src);
-  img.setAttribute('alt', 'avatar');
   const div = document.createElement('div');
   div.classList.add('about');
   const name = document.createElement('div');
   name.classList.add('name');
   name.setAttribute(dataTestid, 'online-user');
-  const status = document.createElement('div');
-  img.classList.add('avatar');
   name.innerText = nickname;
-  status.classList.add('status');
-  const i = document.createElement('i');
-  i.classList.add('fa', 'fa-circle', 'online');
-  status.appendChild(i);
-  const span = document.createElement('span');
-  span.innerText = 'online';
-  status.appendChild(span);
   div.appendChild(name);
-  div.appendChild(status);
-  li.appendChild(img);
+
   li.appendChild(div);
   if (nickname === getUser()) {
     return chatUsers.insertBefore(li, chatUsers.childNodes[0]);
