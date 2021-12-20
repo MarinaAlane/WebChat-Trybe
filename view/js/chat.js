af3a22e05280ab5e8c24c7efa28565e1d427eb8d
Amandha W. Barok <amandha.barok@gmail.com>
2021-12-19 19:37:27 -1000

    Uploading code

--- view/js/chat.js
+++ view/js/chat.js
@@ -5,7 +5,12 @@ const inputMessage = document.querySelector('#messageInput');
 const btnSaveNickname = document.querySelector('#saveNickname');
 const nicknameInp = document.querySelector('#nicknameInput');
 
-let currentNickname = 'Sem nome';
+let currentNickname = [];
+
+function logRandomName(gender) {
+  generateName(gender).then(currentNickname.push());
+  console.log(currentNickname, 'AQUI')
+}
 
 btnSaveNickname.addEventListener('click', () => {
   currentNickname = nicknameInp.value;
@@ -27,3 +32,53 @@ const createMessage = (chatMessage) => {
 };
 
 socket.on('message', (chatMessage) => createMessage(chatMessage));
+
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  } catch (error) {
+    console.error('Unable to fetch data:', error);
+  }
+}
+
+function fetchNames(nameType) {
+  return fetchData(`https://www.randomlists.com/data/names-${nameType}.json`);
+}
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+async function generateName(gender) {
+  try {
+    const response = await Promise.all([
+      fetchNames(gender || pickRandom(['male', 'female'])),
+      fetchNames('surnames')
+    ]);
+
+    const [firstNames, lastNames] = response;
+
+    const firstName = pickRandom(firstNames.data);
+    const lastName = pickRandom(lastNames.data);
+
+    return `${firstName} ${lastName}`;
+  } catch (error) {
+    console.error('Unable to generate name:', error);
+  }
+}
+
+// function logRandomName(gender) {
+//   generateName(gender).then(console.log);
+// }
+
+// logRandomName()
+
+// function returnRandomName(){
+//   generateName(gender).then(console.log)
+// }
+
+// generateName(gender).then(console.log);
