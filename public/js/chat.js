44c51c4529b358585405735547aa409ef4e35e9d
Laura Gusmao <laurargus@gmail.com>
2021-12-21 18:53:34 -0300

    feat: chat script - buttons event listeners

---
+++ public/js/chat.js
@@ -0,0 +1,25 @@
+const socket = window.io();
+
+const saveBtn = document.getElementById('save-btn');
+saveBtn.addEventListener('click', () => {
+  const usernameInput = document.getElementById('username-input');
+  const username = usernameInput.value;
+
+  sessionStorage.setItem('username', username);
+
+  usernameInput.value = '';
+});
+
+const sendBtn = document.getElementById('send-btn');
+sendBtn.addEventListener('click', () => {
+  const username = sessionStorage.getItem('username');
+  const messageInput = document.getElementById('message-input');
+
+  const newMessage = {
+    chatMessage: messageInput.value,
+    nickname: username,   
+  };
+  socket.emit('newMessage', newMessage);
+
+  messageInput.value = '';
+});
