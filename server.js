9641ab4182c4a1d776e5920d4ecc0101ff4b8ec7
Marconi <marconieng@gmail.com>
2021-12-22 18:05:40 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
