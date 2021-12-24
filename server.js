c592cf4777acf73db5a5b526c0441f3c4ec43d0b
lima <joaopaulo.gomeslima8@gmail.com>
2021-12-24 11:33:46 -0300

    Adiciona inicio do servidor.

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const PORT = '3000';
+
+app.listen(PORT, () => {
+  console.log(`Server is Listening on ${PORT}`);
+});
