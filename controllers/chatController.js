7c614f7c28c41549b74fb702dc35115ffd02bcad
Antonio Schappo <antonioschappo@gmail.com>
2021-12-27 18:05:45 -0300

    setting view and socket

---
+++ controllers/chatController.js
@@ -0,0 +1,5 @@
+// const Messages = require('../models/messagesModel');
+
+module.exports = (req, res) => {
+   res.status(200).render('chat', { messages });
+};
--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+const io = require('socket.io')(server, {
+  cors: {
+    origin: `http://localhost:${PORT}`, // url aceita pelo cors
+    methods: ['GET', 'POST', 'PUT'], // Métodos aceitos pela url
+  },
+});
+
+const chatController = require('./controllers/chatController');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+require('./sockets/ping')(io);
+require('./sockets/chat')(io);
+
+app.get('/', chatController);
+
+server.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
+});
