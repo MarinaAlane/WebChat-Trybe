57ec897340c04631baa7362196c7b2218837c3cd
Matheus Gaspar <msouzagaspar@gmail.com>
2021-12-27 13:41:55 -0300

    setup socket.io and finish requirement 1

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 
 const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id}`);
+
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const time = new Date();
+    const day = time.getDay();
+    const month = time.getMonth();
+    const year = time.getFullYear();
+    const date = `${day}-${month}-${year} ${time.toLocaleTimeString('en-US')}`;
+    
+    io.emit('message', `${date} ${nickname} ${chatMessage}`);
+  });
+});
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(3000, () => {
+http.listen(3000, () => {
   console.log('Servidor ouvindo na porta 3000');
 });
