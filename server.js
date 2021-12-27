1f1cede83fbb79f396f6f14830040dff0431fe7c
Matheus Gaspar <msouzagaspar@gmail.com>
2021-12-27 11:09:22 -0300

    setup express server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
