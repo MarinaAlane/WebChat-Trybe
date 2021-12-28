a05df16c4142be820d45d461ba3a117889f734ce
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-12-28 14:55:41 -0300

    Requisito 02 passando nos testes.

--- index.js
+++ index.js
@@ -2,32 +2,50 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const { Server } = require('socket.io');
+const { sendMessage, changeNickname, disconnectUser } = require('./callbacks');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.sendFile(path.join(__dirname, 'views/', 'index.html'));
 });
+
+const usersList = {
+  online: [],
+  addUser(user) {
+    this.online.push(user);
+  },
+  removeUser(id) {
+    const index = this.online.findIndex((userData) => id === userData.id);
+    this.online.splice(index, 1);
+  },
+  changeUserNick(newNick, id) {
+    const index = this.online.findIndex((userData) => id === userData.id);
+    this.online[index].nickname = newNick;
+  },
+};
 
 io.on('connection', (socket) => {
   console.log(`usuário ${socket.id} conectado`);
+  usersList.addUser({ id: socket.id, nickname: socket.id.substring(0, 16) });
+  io.emit('connection', usersList.online);
 
-  socket.on('message', ({ chatMessage, nickname }) => {
-    const now = new Date();
-    const date = now.toLocaleDateString().replace(/\//g, '-');
-    const hour = now.toLocaleTimeString();
+  socket.on('message', ({ chatMessage, nickname }) => { 
+    sendMessage({ chatMessage, nickname }, usersList, io, socket);
+  });
 
-    console.log(`${date} ${hour} ${nickname}: ${chatMessage}`);
-    io.emit('message', `${date} ${hour} ${nickname}: ${chatMessage}`);
+  socket.on('changeNickname', ({ nickname, id }) => {
+    changeNickname({ nickname, id }, usersList, io, socket);
   });
 
   socket.on('disconnect', () => {
-    console.log(`usuário ${socket.id} desconectou`);
+    disconnectUser(usersList, io, socket); 
   });
 });
 
 module.exports = {
   server,
+  io,
 };
