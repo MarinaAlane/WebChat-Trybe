c00fcc44a870a1d4a0abbc4ce3321159d7189200
lima <joaopaulo.gomeslima8@gmail.com>
2021-12-28 00:41:49 -0300

    Adiciona mudanças de logica.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,22 @@
-const socket = window.io();
-// const moment = window.moment();
+//  Estou tendo varias conexoes sockets que n quero. Pesquisei e achei essa materia falando sobre o modo de pesquisa thhp padãro do socket
+//  https://www.ti-enxame.com/pt/javascript/pagina-node.js-socket.io-atualiza-varias-conexoes/830171781/
+const socket = window.io({ transports: ['websocket'], upgrade: false });
 
 const formSendMessage = document.querySelector('#form-chatMessage');
+const formNickname = document.querySelector('#form-nickname');
+const connectionsList = document.querySelector('#connections-list');
 
 function sendMessage(event) {
   event.preventDefault();
 
   const chatMessage = document.querySelector('input[name=chatMessage');
-  const nicknameInput = document.querySelector('input[name=nickname');
 
   socket.emit('message', {
     chatMessage: chatMessage.value,
-    nickname: nicknameInput.value,
+    nickname: socket.id,
   });
 
   chatMessage.value = '';
-  nicknameInput.value = '';
 }
 
 function reciveMessage(msg) {
@@ -31,22 +32,41 @@ function reciveMessage(msg) {
 function saveNickname(nickname) {
   const li = document.createElement('li');
   li.setAttribute('data-testid', 'online-user');
+  li.setAttribute('name', `${nickname}`);
   li.append(nickname);
 
-  const connectionsList = document.querySelector('#connections-list');
   connectionsList.appendChild(li);
 }
 
+//  Buscar deletar essa função - Não deve ser necessario apagar tudo para depois renderizar novamente
+// function removeAllNicknames() {
+//   // Para remover todos os childrens da lista de usuarios usei a seguinte referencia:
+//   // https://www.geeksforgeeks.org/remove-all-the-child-elements-of-a-dom-node-in-javascript/
+//   let child = connectionsList.lastElementChild;
+//   while (child) {
+//     connectionsList.removeChild(child);
+//     child = connectionsList.lastElementChild;
+//   }
+// }
+
+function setNickname(event) {
+  event.preventDefault();
+
+  const newNickname = document.querySelector('input[name=nickname');
+  socket.emit('setNickname', newNickname.value);
+  newNickname.value = '';
+}
+
+socket.on('message', (msg) => reciveMessage(msg));
+
+socket.on('nickname', (nickname) => {
+  // removeAllNicknames(); // Mudar essa logica para nõ deletar todos os nomes e depois reescrevee
+  const oldNickname = document.querySelector(
+    `li[name=${socket.id.substr(0, 16)}]`
+  );
+  if (oldNickname) connectionsList.removeChild(oldNickname);
+  saveNickname(nickname);
+});
+
 formSendMessage.addEventListener('submit', sendMessage);
-
-socket.on('message', (msg) => {
-  console.log('message socket.on acionada (chat.js)', msg);
-  reciveMessage(msg);
-});
-
-socket.on('nickname', ({ userList }) => {
-  console.log('nickname socket.on acionada (chat.js)', userList);
-  userList.forEach((nickname) => {
-    saveNickname(nickname);
-  });
-});
+formNickname.addEventListener('submit', setNickname);
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || '3000';
 
 const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
+const io = require('socket.io')(server, { 
   cors: {
     origin: `http://localhost:${PORT}`,
     methods: ['GET', 'POST'],
@@ -22,41 +22,34 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/chat.html'));
 }); // envia formulario quando cliente conecta com a rota
 
-const userList = [];
+const users = {};
 
 io.on('connection', (socket) => {
-  console.log(`Novo usuário ${socket.id} conectado ao socket.io`);
+  users[socket.id] = socket.id.substr(0, 16); // Define um nome aleatorio para cada usuario na chave de seu id.
+  console.log(`Novo usuário ${users[socket.id]} conectado ao socket.io`);
+  console.log(`oooooooooo ${users} `);
 
-  userList.push(socket.id.substr(0, 16));
-  console.log(`Novo usuário ${userList} conectado ao socket.io`);
-  console.log(`Quantidade: ${userList.length} `);
-
-  io.emit('nickname', { userList });
+  io.emit('nickname', users[socket.id]); // "servidor, envie a lista de usuarios para todos os sockets conectados"
 
   socket.on('message', (msg) => {
     const { chatMessage, nickname } = msg;
     const fullDate = getDate();
 
-    io.emit('message', `${fullDate} - ${nickname}: ${chatMessage}`);
+    io.emit('message', `${fullDate} - ${users[nickname]}: ${chatMessage}`);
+  });
 
-    console.log(`(socket.on server) ${fullDate} - ${nickname}: ${chatMessage}`); // deletar
+  socket.on('setNickname', (newNickname) => {
+    users[socket.id] = newNickname;
+    io.emit('nickname', newNickname);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Usuário ${socket.id} desconectado. (server.js)`);
+    delete users[socket.id];
+
+    io.emit('nickname', users[socket.id]);
   });
 });
-
-// io.on('disconnect', (socket) => {
-//   console.log(`Novo usuário ${socket.id} conectado ao socket.io`);
-// });
-
-// app.post('/message', (req, res) => {
-//   const { chatMessage, nickname } = req.body;
-
-//   const data = moment().locale('pt-br').format('L').replace(/\//g, '-');
-//   const time = moment().format('LTS');
-
-//   io.emit('message', `${data} ${time} - ${nickname}: ${chatMessage}`);
-
-//   return res.status(200).json({ chatMessage, nickname });
-// });
 
 server.listen(PORT, () => {
   console.log(`Server is Listening on ${PORT}`);
