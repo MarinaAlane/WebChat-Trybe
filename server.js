2a73f6980109c43b0230822cb8ef40f6552b8945
Wilquer82 <wilquer.fig@gmail.com>
2021-12-29 17:07:03 -0300

    lint errors 2

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.use('/', (req, res) => {
-  res.render('index.html')
-})
+  res.render('index.html');
+});
 
 require('./sockets/chat')(socket);
 
