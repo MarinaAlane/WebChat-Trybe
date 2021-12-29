083fe1fd47778b57d7cf9d9ba36d328fa4b1e8c3
Wilquer82 <wilquer.fig@gmail.com>
2021-12-29 17:05:07 -0300

    lint errors

--- server.js
+++ server.js
@@ -29,10 +29,11 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
+
 app.set('view engine', 'html');
 
 app.use('/', (req, res) => {
-  res.render('index.html');
+  res.render('index.html')
 })
 
 require('./sockets/chat')(socket);
