37cead09bf2b0f33c4df5c570c7284337454544b
Reginara Gomes <reginara.gomes@datasprints.com>
2021-12-29 21:02:06 -0300

    Inicia o requisito 4. Não passa nos testes ainda

--- server.js
+++ server.js
@@ -14,12 +14,28 @@ const io = require('socket.io')(server, {
     methods: ['GET', 'POST'],
   } });
 
+  let onlineUser = [];
+
 // é um escutador, escuta o que está no 'remetente/cliente' - escuta a conexão de forma geral
 io.on('connection', (socket) => {
   const userId = socket.id.slice(0, 16);
 
   socket.emit('connectedUser', userId);
   console.log(userId);
+
+  socket.on('saveNickname', (nicknameId) => {
+    onlineUser = onlineUser.filter((user) => user.id !== socket.id);
+
+    // para o requisito 4, tive ajuda do meu colega Marcelo Leite - Turma 11, na elaboração da ordenação dos id's de usuários. Usamos a HOF sort por ser aquela que mais se aproximava do que o requisito pedia, e por na sua documentação, ter um exemplo que fosse similar de comparações e ordenações.
+    onlineUser.push({ position: socket.id, nicknameId });
+    console.log(onlineUser);
+    io.emit('allLogged', onlineUser);
+  });
+
+  socket.on('disconnect', () => {
+    onlineUser = onlineUser.filter((user) => user.id !== socket.id);
+    io.emit('allLogged', onlineUser);
+  });
 
   // escuta a msg de quem está enviando (tipo uma msg privada)
   socket.on('message', (info) => {
