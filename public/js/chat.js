e6227dfe7f2b11139e8c710170571a1338fa885f
AntonioSchappo <antonioschappo@gmail.com>
2021-12-30 11:39:07 -0300

    implementing change nick functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,12 @@
 const socket = window.io();
 
 const testid = 'data-testid';
-const form = document.querySelector('#send-container');
-const input = document.querySelector('#message-input');
+const messageForm = document.querySelector('#send-container');
+const messageInput = document.querySelector('#message-input');
 const messages = document.querySelector('#messages');
+const nicknameForm = document.querySelector('#nickname-form');
 const nicknameList = document.querySelector('#nickname-list');
+const nicknameInput = document.querySelector('#nickname-input');
 
 const createListItem = (value, list, attribute = [null, null]) => {
     const listedItem = document.createElement('li');
@@ -14,30 +16,38 @@ const createListItem = (value, list, attribute = [null, null]) => {
     window.scrollTo(0, document.body.scrollHeight);
 };
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault(); // previne recarregamento da página
-    if (input.value) {
-        socket.emit('message', { chatMessage: input.value, nickname: socket.id });
-    }
-    input.value = '';
-});
-
-const formNickname = (value) => value.slice(0, 16);
+const makeNickname = (value) => value.slice(0, 16);
 
 socket.on('connect', () => {
-    const nickname = formNickname(socket.id);
-    console.log(nickname.length);
+    const nickname = makeNickname(socket.id);
+    sessionStorage.setItem('nickname', nickname);
     socket.emit('newUser', nickname);
 });
 
-socket.on('updateUserList', (users) => {
+socket.on('updateUsersList', (users) => {
     const { [socket.id]: newUser, ...previousUsers } = users;
     nicknameList.innerHTML = '';
-    createListItem(newUser, nicknameList, [testid, 'online-user']);
+    createListItem(newUser, nicknameList, [testid, 'online-user']); // creates the li of the user who logged
     Object.values(previousUsers)
-    .forEach((user) => createListItem(user, nicknameList, [testid, 'online-user']));
+    .forEach((user) => createListItem(user, nicknameList, [testid, 'online-user'])); // creates the li´s for the remaining users stored in the backend object
 });
 
-// });
-            
+nicknameForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // prevents the page from reloading
+    if (nicknameInput.value) {
+        socket.emit('updateNickname', nicknameInput.value);
+        sessionStorage.setItem('nickname', nicknameInput.value);
+    }
+    nicknameInput.value = '';
+});
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // prevents the page from reloading
+    if (messageInput.value) {
+        socket.emit('message',
+        { chatMessage: messageInput.value, nickname: sessionStorage.getItem('nickname') });
+    }
+    messageInput.value = '';
+});
+
 socket.on('message', (msg) => { createListItem(msg, messages, [testid, 'message']); });
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,15 +1,22 @@
-const users = {}; // Store users in object in order to manage connect
+const users = {}; // Stores users in object in order to manage connect
                     // and disconnect listing and nickname updates
 
 module.exports = (io) => io.on('connection', (socket) => {
     socket.on('newUser', (nickname) => {
         users[socket.id] = nickname;
-        io.emit('updateUserList', users);
+        io.emit('updateUsersList', users);
     });
+
+    socket.on('updateNickname', (newNickname) => {
+        users[socket.id] = newNickname;
+        io.emit('updateUsersList', users);
+    });
+
     socket.on('message', ({ chatMessage, nickname }) => {
         const todayDate = new Date().toLocaleString().replaceAll('/', '-');
         io.emit('message', `${todayDate} ${nickname}: ${chatMessage}`);
     });
+
     socket.on('disconnect', () => {
         delete users[socket.id];
         io.emit('updateUserList', users);
