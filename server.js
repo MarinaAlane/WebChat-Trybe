62f4f08bbda643df5d9a9b9187ff363c9109210a
dcmatheus <mathws.costag@gmail.com>
2021-12-31 04:32:15 -0300

    configura o server

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const message = require('./routes/message');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  } });
 
-app.set('views', './views');
+app.use(express.static(`${__dirname}/public`));
+
+app.set('view engine', 'ejs');
+app.set('views', './public');
+
+require('./sockets/chat')(io);
+
+const message = require('./routes/message');
 
 app.use('/', message);
 
