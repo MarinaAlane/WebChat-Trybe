4f1dcfc1991b1f7830a7b54e50e1e39bffdc1cc3
Johnny Rubi <johnnyrubischreiner@gmail.com>
2022-01-01 20:01:49 -0300

    chat funcionando

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 // Faça seu código aqui
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const PORT = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuario ${socket.id} conectado`);
+});
+
+server.listen(PORT, () => console.log(`estou escutando na porta ${PORT}`));
