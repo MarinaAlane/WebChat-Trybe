e86a649cf0cbd4774a70586218786f8a4dad6ee7
IsaacMagno <isaacmagno98@gmail.com>
2022-01-02 12:39:38 -0300

    desafio1

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
-// Faça seu código aqui
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log('Alguem se desconectou');
+  });
+
+  socket.on('message', (chatMessage, nickname) => {
+    io.emit('message', { chatMessage, nickname });
+  });
+
+  // socket.emit('welcome', ('Seja bem vindo(a) ao Web Chat'));
+
+  socket.broadcast.emit('serverMessage', { message: 'Uma nova pessoa se conectou' });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
+
+// require('./sockets/chat')(io);
