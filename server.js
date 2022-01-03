601270c3cbb04862a629dd8fc2c27c66b021db82
IsaacMagno <isaacmagno98@gmail.com>
2022-01-03 10:20:49 -0300

    desafio2completed

--- server.js
+++ server.js
@@ -12,9 +12,27 @@ const io = require('socket.io')(http, {
 
 app.use(cors());
 
+const users = [];
+
+const updateUsers = (updateUser) => {
+  const findUser = users.findIndex((user) => user.id === updateUser.id);
+  users.splice(findUser, 1);
+  users.push(updateUser);
+  return users;
+};
+
 io.on('connection', (socket) => {
+  const userConnected = { id: socket.id, nickname: socket.id.substring(0, 16) };
+  users.push(userConnected);
+  socket.emit('online-user', users);
+
   socket.on('disconnect', () => {
     console.log('Alguem se desconectou');
+  });
+
+  socket.on('newNickname', (nickName) => {
+    const userUpdated = updateUsers(nickName);
+    io.emit('online-user', userUpdated);
   });
 
   socket.on('message', ({ chatMessage, nickname }) => {
@@ -23,10 +41,6 @@ io.on('connection', (socket) => {
 
     io.emit('message', message);
   });
-
-  // socket.emit('welcome', ('Seja bem vindo(a) ao Web Chat'));
-
-  socket.broadcast.emit('serverMessage', { message: 'Uma nova pessoa se conectou' });
 });
 
 app.get('/', (req, res) => {
@@ -36,5 +50,3 @@ app.get('/', (req, res) => {
 http.listen(3000, () => {
   console.log('Servidor ouvindo na porta 3000');
 });
-
-// require('./sockets/chat')(io);
