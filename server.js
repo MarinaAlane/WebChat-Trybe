c6c57050a8d3609d328e289db947f1adf3065faf
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2022-01-03 11:09:41 -0300

    Requisito 01 - Fix: Adicionando comentarios de referencia

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.get('/', (_req, res) => {
 
 io.on('connection', (socket) => {
   console.log(`Usuário ${socket.id} conectado`);
+/*
+  A função Date aprendi com o colega Alberto Candito
+  https://github.com/tryber/sd-011-project-webchat/pull/45
+  A variavel data message recebe um obj. do tipo date que é passível de ser
+  manipulado pelas funções toLocaleDataString e toLocaleTimeString. Na linha
+  23 o uso do replace procura, atraves de um regex, por ocorrencias na string desejada
+  e substitui pelo sinal '-'. Na linha 24 o objeto entre parenteses seta o horario
+  utilizado como padrão de 12 horas e mostra AM ou PM para definir o período do dia
+   */
   const dataMessage = new Date();
   const onlyDate = dataMessage.toLocaleDateString('pt-BR').replace(/\//g, '-');
   const onlyTime = dataMessage.toLocaleTimeString('pt-BR', { hour12: true });
