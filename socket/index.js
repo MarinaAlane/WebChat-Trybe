caaeebfa98545908d74acfed984b13b230de29fc
Víctor Faria <vitilevi@hotmail.com>
2022-01-02 22:39:06 -0300

    Corrigido estrutura do server

--- socket/index.js
+++ socket/index.js
@@ -1,19 +1,14 @@
 const getDate = require('../utils/getDate');
 
-const handleConnection = (socket) => {
-  socket.emit('getNickName', socket.id.slice(0, 16));
-};
-
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    handleConnection(socket);
+    socket.emit('getNickName', socket.id.slice(0, 16));
 
     socket.on('updateNickName', (nickName) => {
       socket.emit('getNickName', nickName);
     });
   
     socket.on('message', ({ nickname, chatMessage }) => {
-      console.log(nickname, chatMessage);
       const date = getDate();
       io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
     });
