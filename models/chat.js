2a95bbdd091ef7e9459c1982bd19cfdcdde77244
RobsonCardozo <robsonleaocardozo@hotmail.com>
2022-01-03 15:24:59 -0300

    getChatHistory function added

--- models/chat.js
+++ models/chat.js
@@ -1,8 +1,18 @@
 const connection = require('./connection');
 
+const getChatHistory = async () => {
+  const db = await connection();
+  const history = await db.collection('messages').find().toArray();
+
+  return history;
+};
+
 const chatHistory = async (message) => {
   const db = await connection();
   await db.collection('message').insertOne(message);
 };
 
-module.exports = chatHistory;
+module.exports = {
+  chatHistory,
+  getChatHistory,
+};
