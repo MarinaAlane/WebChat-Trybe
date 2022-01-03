df5b063cf874bc56977d262fced13fcfbceb6736
Guilherme Nunes <guiicnunes@gmail.com>
2022-01-03 10:19:41 -0300

    FEAT: Requisito 2

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,37 @@
 const socket = window.io();
 
-const form = document.querySelector('form');
+const formMsg = document.querySelector('#message');
+const formNick = document.querySelector('#nick');
 const nick = document.querySelector('#nickName');
+const nickBox = document.querySelector('#nick-name-box');
 const msg = document.querySelector('#messageInput');
 
-form.addEventListener('submit', (e) => {
+const getRandomNumb = () => {
+  let result = Math.floor(Math.random() * 75) + 48;
+  if (
+      (result > 57 && result < 65)
+      || (result > 90 && result < 97)
+  ) result = getRandomNumb();
+
+  return result;
+};
+
+function getRandomNick() {
+  const arrOfRandNumb = Array.from({ length: 16 }, getRandomNumb);
+  const newNick = String.fromCharCode(...arrOfRandNumb);
+  
+  return newNick;
+}
+
+if (nick.innerText === '') nick.innerText = getRandomNick();
+formNick.addEventListener('submit', (e) => {
   e.preventDefault();
-  if (nick.value === '') nick.value = socket.id;
-  socket.emit('message', { chatMessage: msg.value, nickname: nick.value });
+  nick.innerText = nickBox.value;
+});
+
+formMsg.addEventListener('submit', (e) => {
+  e.preventDefault();
+  socket.emit('message', { chatMessage: msg.value, nickname: nick.innerText });
   msg.value = '';
   return false;
 });
@@ -16,6 +40,7 @@ const createMessage = (message) => {
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
   li.innerText = message;
+  li.setAttribute('data-testid', 'message');
   messagesUl.appendChild(li);
 };
 
