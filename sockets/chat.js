22bd472bc66db5820b427b7298d5f2c203de7e2a
Juliana Happatsch <jhappatsch@gmail.com>
2022-01-02 22:19:53 -0300

    que ódio esqueci de mudar o nome pra message e tava aq morrendo jesus apaga a luz

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,12 +1,11 @@
 const moment = require('moment');
 
 module.exports = (io) => io.on('connection', (socket) => {
-  // socket.broadcast.emit('hello', `Iiiiiirraaaa! ${socket.id} acabou de se conectar :D`);
   console.log(`Usuário ${socket.id} acabou de se conectar`);
 
   socket.on('message', ({ nickname, chatMessage }) => {
     console.log(`Mensagem ${chatMessage}`);
-    io.emit('serverMessage', 
+    io.emit('message', 
       `${moment().format('DD-MM-yyyy HH:mm:ss A')} - ${nickname}: ${chatMessage}`);
   });
 
@@ -16,7 +15,6 @@ module.exports = (io) => io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    // socket.broadcast.emit('serverMessage', `Xiii! ${socket.id} acabou de se desconectar! :(`);
     console.log(`Usuário ${socket.id} acabou de se desconectar`);
   });
 });
