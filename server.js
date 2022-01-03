2ed05b1a295aad20c9310649909a0fc4982aae1f
RobsonCardozo <robsonleaocardozo@hotmail.com>
2022-01-03 15:04:50 -0300

    fixes and server.js implementation

--- server.js
+++ server.js
@@ -5,8 +5,26 @@ require('dotenv').config();
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
+  cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] },
+});
+
+const chat = require('./models/chat');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (_req, res) => 
+  res.render('index'));
+
+// const users = {};
+const timestamp = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+io.on('connection', (socket) => {
+  socket.on('message', async (userMessage) => {
+    const { message, nickname } = userMessage;
+    await chat.savedHistory({ nickname, message, timestamp });
+    io.emit('message', `${timestamp} - ${nickname}: ${message}`);
+  });
 });
