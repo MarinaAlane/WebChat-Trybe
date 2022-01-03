b6b23fe42103b4a3864b2dc26d0b370f3a408a74
Maran <maranmorimoto@gmail.com>
2022-01-03 12:16:12 -0300

    req. 04 - implement users list

--- server.js
+++ server.js
@@ -15,15 +15,32 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+let onlineUsers = [];
 io.on('connection', (socket) => {
-  console.log(socket.id);
+  socket.emit('userOnline', socket.id.slice(0, 16));
+  console.log(onlineUsers);
+
+  socket.on('nick', (nickname) => {
+    onlineUsers = onlineUsers.filter((user) => user.id !== socket.id);
+    onlineUsers.push({ nickname, id: socket.id });
+    io.emit('user', onlineUsers);
+  });
+
   socket.on('message', (msg) => { 
     // socket.on escuta somente a mensagem do usuario emissor.
       const date = moment().format('DD-MM-YYYY HH:mm:ss');
       const chatLine = `${date} - ${msg.nickname}: ${msg.chatMessage}`;
       io.emit('message', chatLine); // io.emit emite a todos a mensagem que foi enviada por um usuario
   });
-  socket.emit('user', socket.id.slice(0, 16));
+  
+  // onlineUsers = onlineUsers.filter((user) => user.id !== socket.id);
+    
+  socket.on('disconnect', () => {
+    console.log('usuario desconetou')
+    onlineUsers = onlineUsers.filter((user) => user.id !== socket.id);
+    console.log(onlineUsers);
+    io.emit('user', onlineUsers);
+  });
 });
 
 server.listen(3000, () => {
