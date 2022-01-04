095956176d2b6bdc4240322feee17758fe06951a
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-01-04 09:08:39 -0300

    fix: insert message into its list

--- views/Script/messageScript.js
+++ views/Script/messageScript.js
@@ -20,49 +20,13 @@ messageBtn.addEventListener('click', () => {
 
 // ====================================================================================================
 
-const messageList = document.getElementById('messages');
+// ================================== insert message into its list ===========================================
 
-const insertIntoMessageList = (messages) => {
-  messages.forEach(({ timestamp, nickname, message }) => {
-    const msg = document.createElement('li');
-    msg.textContent = `${timestamp} - ${nickname}: ${message}`;
-    msg.setAttribute('data-testid', 'message');
-    messageList.appendChild(msg);
-    window.scrollTo(0, document.body.scrollHeight);
-  });
-};
+socket.on('message', (data) => {
+  const messagesList = document.getElementById('messages');
+  const message = document.createElement('li');
+  message.setAttribute('dataTestId', 'message');
+  message.innerText = data;
 
-const listAllMessages = () => {
-  socket.on('messageHistory', (allMessages) => {
-    messageList.innerHTML = '';
-    insertIntoMessageList(allMessages);
-  });
-};
-
-listAllMessages();
-
-const form = document.getElementById('form');
-const input = document.getElementById('input');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (input.value) {
-    // const chatMessage = input.value;
-    // socket.emit('message', { nickname, chatMessage });
-    input.value = '';
-  }
-});
-
-// ..source: https://stackoverflow.com/questions/18648500/add-id-class-to-objects-from-createelement-method
-const messagesHistory = [];
-
-socket.on('message', (msg) => {
-  const messages = document.getElementById('messages');
-  const item = document.createElement('li');
-  item.textContent = msg;
-  messagesHistory.push(msg);
-  item.setAttribute('data-testid', 'message');
-  sessionStorage.setItem('messages', messagesHistory);
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
+  messagesList.appendChild(message);
 });
