9339ccbca8e984dab297a33763e7826dba676ea4
Matheus Antonio <matheusantonio232@gmail.com>
2022-01-04 16:16:29 -0300

    iniciando projeto

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-// Faça seu código aqui
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+server.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
