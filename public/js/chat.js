8c8e944dba4fd32150fdd89c5d34f8865bfb6980
Víctor Faria <vitilevi@hotmail.com>
2022-01-03 21:50:59 -0300

    Adiciona requisito 4

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,14 +4,11 @@ const nickName = document.querySelector('#nickName');
 const nickBtn = document.querySelector('#nickBtn');
 const messageInput = document.querySelector('#textMessage');
 const submitBtn = document.querySelector('#submitMessage');
+const onlineUsers = document.querySelector('.online-users');
 
 const setSessionStorage = (nick) => sessionStorage.setItem('webChatNickName', nick);
 
 const getSessionStorage = () => sessionStorage.getItem('webChatNickName');
-
-socket.on('getNickName', ((nick) => {
-  nickName.innerHTML = nick;
-}));
 
 const displayNewMessage = (message) => {
   const messages = document.querySelector('.messages');
@@ -37,7 +34,26 @@ nickBtn.addEventListener('click', (e) => {
   nick.value = '';
 });
 
+const handleOnlineUsers = (nick) => {
+  const li = document.createElement('li');
+  li.setAttribute('data-testid', 'online-user');
+  li.innerText = nick;
+  onlineUsers.appendChild(li);
+};
+
+socket.on('getNickName', ((nick) => {
+  nickName.innerHTML = nick;
+}));
+
 socket.on('message', (message) => displayNewMessage(message));
+
+socket.on('onlineUsers', (users) => {
+  onlineUsers.innerHTML = '';
+  const userList = Object.values(users);
+  const index = userList.indexOf(nickName.innerText);
+  handleOnlineUsers(userList[index]);
+  userList.filter((user) => user !== userList[index]).forEach((nick) => handleOnlineUsers(nick));
+});
 
 window.onload = () => {
   const nick = getSessionStorage();
--- socket/index.js
+++ socket/index.js
@@ -1,12 +1,25 @@
 const getDate = require('../utils/getDate');
 const { newMessage } = require('../models/messages');
 
+const users = {};
+
+const handleConnection = (socket, io) => {
+  const randomNickName = socket.id.slice(0, 16);
+  socket.emit('getNickName', randomNickName);
+
+  users[socket.id] = randomNickName.toString();
+  io.emit('onlineUsers', users);
+};
+
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    socket.emit('getNickName', socket.id.slice(0, 16));
+    handleConnection(socket, io);
+    console.log(users);
 
     socket.on('updateNickName', (nickName) => {
       socket.emit('getNickName', nickName);
+      users[socket.id] = nickName;
+      io.emit('onlineUsers', users);
     });
   
     socket.on('message', ({ nickname, chatMessage }) => {
@@ -16,7 +29,8 @@ module.exports = (io) => {
     });
 
     socket.on('disconnect', () => {
-      console.log('disconectou jovem');
+      delete users[socket.id];
+      io.emit('onlineUsers', users);
     });
   });
 };
