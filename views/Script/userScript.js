1acac79f600d21868e073c1958f9034cacb620a7
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2022-01-04 08:30:50 -0300

    refactor: sepate script user and message

--- views/Script/userScript.js
+++ views/Script/userScript.js
@@ -1,26 +1,10 @@
 const socket = window.io();
 
-const messageList = document.getElementById('messages');
 const onlineUserList = document.getElementById('users');
 
-const insertIntoMessageList = (messages) => {
-  messages.forEach(({ timestamp, nickname, message }) => {
-    const msg = document.createElement('li');
-    msg.textContent = `${timestamp} - ${nickname}: ${message}`;
-    msg.setAttribute('data-testid', 'message');
-    messageList.appendChild(msg);
-    window.scrollTo(0, document.body.scrollHeight);
-  });
-};
+// ================================== User Form manipulation ===========================================
 
-const listAllMessages = () => {
-  socket.on('messageHistory', (allMessages) => {
-    messageList.innerHTML = '';
-    insertIntoMessageList(allMessages);
-  });
-};
-
-listAllMessages();
+// ====================================================================================================
 
 // ================================== Create User =====================================================
 
@@ -102,6 +86,8 @@ socket.on('changeUserName', ({ oldNickname, newNickname }) => {
   }
 });
 
+// ====================================================================================================
+
 // ..source: https://www.geeksforgeeks.org/how-to-detect-browser-or-tab-closing-in-javascript/
 // window.addEventListener('beforeunload', (e) => {
 //     e.preventDefault();
@@ -110,29 +96,3 @@ socket.on('changeUserName', ({ oldNickname, newNickname }) => {
 //     e.returnValue = '';
 //     listAllUsersOnline();
 // });
-
-const form = document.getElementById('form');
-const input = document.getElementById('input');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (input.value) {
-    // const chatMessage = input.value;
-    // socket.emit('message', { nickname, chatMessage });
-    input.value = '';
-  }
-});
-
-// ..source: https://stackoverflow.com/questions/18648500/add-id-class-to-objects-from-createelement-method
-const messagesHistory = [];
-
-socket.on('message', (msg) => {
-  const messages = document.getElementById('messages');
-  const item = document.createElement('li');
-  item.textContent = msg;
-  messagesHistory.push(msg);
-  item.setAttribute('data-testid', 'message');
-  sessionStorage.setItem('messages', messagesHistory);
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
-});
