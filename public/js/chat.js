7867efb38b8d1cd4d8240824bad16b263638a162
Marconi <marconieng@gmail.com>
2022-01-05 13:17:05 -0300

    Atualizaçao do status do projeto

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,28 @@
 const socket = window.io();
 
-const form = document.querySelector('form');
+const formClient = document.querySelector('#client-form');
+const inputNickname = document.querySelector('#clientInput');
+
+formClient.addEventListener('submit', (e) => {
+  e.preventDefault();
+  socket.emit('nickname', inputNickname.value);
+  inputNickname.value = '';
+  return false;
+});
+
+const saveNickname = (nickname) => {
+  const clientNameUl = document.querySelector('#client');
+  const li = document.createElement('li');
+  li.innerText = nickname;
+  clientNameUl.appendChild(li);
+};
+
+socket.on('nickname', (nickname) => saveNickname(nickname));
+
+const formMessages = document.querySelector('#form-messages');
 const inputMessage = document.querySelector('#messageInput');
 
-form.addEventListener('submit', (e) => {
+formMessages.addEventListener('submit', (e) => {
   e.preventDefault();
   socket.emit('message', {
     chatMessage: inputMessage.value,
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,13 +1,16 @@
 module.exports = (io) => io.on('connection', (socket) => {
-  socket.broadcast.emit('serverMessage', `${socket.id} acabou de se conectar :D`);
+  socket.broadcast.emit('serverMessage', console.log(`${socket.id} acabou de se conectar :D`));
 
+  // https://stackoverflow.com/questions/10632346/how-to-format-a-date-in-mm-dd-yyyy-hhmmss-format-in-javascript
   socket.on('message', (message) => {
-    // https://stackoverflow.com/questions/10632346/how-to-format-a-date-in-mm-dd-yyyy-hhmmss-format-in-javascript
-
     const date = new Date();
     const dateFormat = [date.getDate(), date.getMonth() + 1, date.getFullYear()].join('-');
     const hourFormat = [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
 
     io.emit('message', `${dateFormat} ${hourFormat} - ${message.nickname}: ${message.chatMessage}`);
+  });
+
+  socket.on('nickname', (nickname) => {
+    io.emit('nickname', nickname);
   });
 });
