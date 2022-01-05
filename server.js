f092c0219cc7689975a0ad08201bcb4d52901632
Wilquer82 <wilquer.fig@gmail.com>
2022-01-05 03:22:56 -0300

    Acho que agora vai9

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const path = require('path');
+const cors = require('cors');
 
 require('./sockets/chat')(io);
 
 app.use(express.static('views'));
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
+app.use(cors());
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
 app.get('/', (_, res) => {
     res.render('chat');
 });
