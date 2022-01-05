9af7a9d4cf6a3c58ead31ac4d680c73f3173c51d
Wilquer82 <wilquer.fig@gmail.com>
2022-01-05 04:09:00 -0300

    Acho que agora vai35

--- server.js
+++ server.js
@@ -1,22 +1,47 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+const server = require('http').createServer(app);
+
+const urlOrigin = `http://localhost:${PORT}`;
+
+const io = require('socket.io')(server, {
+
+  cors: {
+
+    methods: ['GET', 'POST'],
+    
+    origin: urlOrigin,
+
+  },
+
+});
+
+app.use(cors());
 
 require('./sockets/chat')(io);
 
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'views')));
+
 app.set('views', path.join(__dirname, 'views'));
+
 app.engine('html', require('ejs').renderFile);
 
 app.set('view engine', 'ejs');
 
+app.use('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+// app.get('/', (_req, res) => res.render('chat'));
+
 server.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Porta ${PORT}`);
 });
