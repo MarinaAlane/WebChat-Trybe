81d456896c805924f8558c1a3628f9f03ad455d3
Wilquer82 <wilquer.fig@gmail.com>
2022-01-05 03:25:26 -0300

    Acho que agora vai19

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ const cors = require('cors');
 require('./sockets/chat')(io);
 
 app.use(express.static('views'));
+
 app.use(cors());
+
 app.set('views', path.join(__dirname, 'views'));
+
 app.engine('html', require('ejs').renderFile);
+
 app.get('/', (_, res) => {
     res.render('chat');
 });
