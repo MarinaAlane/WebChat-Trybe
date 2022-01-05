99bbe6d36a8ebf979ba7ced7883ed9781c4d18af
vitorguima <vitorguima@gmail.com>
2022-01-04 23:33:28 -0300

    implement some changes

---
+++ public/js/message.js
@@ -0,0 +1,46 @@
+const socket = window.io();
+
+const userNicknameBox = document.querySelector('#online-user');
+const nickname = [...Array(16)].map(() => Math.floor(Math.random() * 10)).join('');
+userNicknameBox.innerText = nickname;
+window.sessionStorage.setItem('nickname', nickname);
+socket.emit('connectUser', { nickname });
+
+const form = document.querySelector('form');
+const inputMessage = document.querySelector('#message-box');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nick = window.sessionStorage.getItem('nickname');
+  socket.emit('message', { nickname: nick, chatMessage: inputMessage.value });
+  inputMessage.value = '';
+  return false;
+});
+
+const messagesList = document.querySelector('#messages-list');
+socket.on('message', (message) => {
+  const li = document.createElement('li');
+  li.innerText = message;
+  li.setAttribute('data-testid', 'message');
+  messagesList.appendChild(li);
+});
+
+const nicknameBox = document.querySelector('#nickname-box');
+const nicknameButton = document.querySelector('#nickname-button');
+nicknameButton.addEventListener('click', () => {
+  window.sessionStorage.setItem('nickname', nicknameBox.value);
+  socket.emit('connectUser', { nickname: nicknameBox.value });
+  userNicknameBox.innerText = nicknameBox.value;
+  nicknameBox.value = '';
+});
+
+const usersList = document.querySelector('#online-users-list');
+socket.on('updateConnectedUsers', (connectedUsers) => {
+  usersList.innerHTML = '';
+  connectedUsers.forEach((user) => {
+    if (user.id === socket.id) return null;
+    const li = document.createElement('li');
+    li.innerText = user.nickname;
+    li.setAttribute('data-testid', 'online-user');
+    usersList.appendChild(li);
+  });
+});
--- server.js
+++ server.js
@@ -5,24 +5,60 @@ const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:3000', // url aceita pelo cors
-    methods: ['GET', 'POST'], // Métodos aceitos pela url
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
   },
 });
 
+const Message = require('./models/Message');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.use(express.static(`${__dirname}/public`));
 
-// require('./sockets/ping')(io);
-require('./sockets/chat')(io);
-// require('./sockets/rooms')(io);
+const connectedUsers = [];
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/public/chat.html`);
+const getDate = () => {
+  const time = new Date();
+  const day = time.getDay();
+  const month = time.getMonth();
+  const year = time.getFullYear();
+  return `${day}-${month}-${year} ${time.toLocaleTimeString('en-US')}`;
+};
+
+const onConnectUser = (socket) => ({ nickname }) => {
+    const index = connectedUsers.findIndex((user) => user.id === socket.id);
+    if (index === -1) {
+ connectedUsers.push({ id: socket.id, nickname });
+    } else {
+      connectedUsers[index] = { id: socket.id, nickname };
+    }
+    io.emit('updateConnectedUsers', connectedUsers);
+  };
+
+io.on('connection', (socket) => {
+  socket.on('connectUser', onConnectUser(socket));
+
+  socket.on('message', ({ chatMessage, nickname }) => {
+    const date = getDate();
+    Message.create({ date, nickname, chatMessage });
+    io.emit('message', `${date} ${nickname} ${chatMessage}`);
+  });
+
+  socket.on('disconnect', () => {
+    const index = connectedUsers.findIndex((user) => user.id === socket.id);
+    connectedUsers.splice(index, 1);
+    io.emit('updateConnectedUsers', connectedUsers);
+  });
 });
 
-// app.get('/chat', (req, res) => {
-//   res.sendFile(__dirname + '/public/chat.html');
-// });
+app.get('/', async (req, res) => {
+  const messages = await Message.getAll();
+  const formattedMessages = messages
+    .map(({ date, nickname, chatMessage }) => `${date} ${nickname} ${chatMessage}`);
+  res.status(200).render('index', { messages: formattedMessages, connectedUsers });
+});
 
 http.listen(3000, () => {
   console.log('Servidor ouvindo na porta 3000');
