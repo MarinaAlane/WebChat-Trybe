e678fd1bbc67d2621d625b7f6bca80af952b7f41
theusseveen22 <mateus.krauss@gmail.com>
2022-01-05 10:13:46 -0300

    Primeiro requisito feito

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-// Faça seu código aqui
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', (socket) => {
+    const data = new Date();
+
+    const dia = data.getDate();
+    const mes = data.getMonth() + 1;
+    const ano = data.getFullYear();
+    const hora = data.getHours();
+    const min = data.getMinutes();
+    console.log(mes);
+    const amOrPm = hora >= 12 ? 'PM' : 'AM';
+    const formatDate = `${dia}-${mes}-${ano} ${hora}:${min} ${amOrPm}`;
+
+    socket.on('message', (msg) => {
+        const { nickname, chatMessage } = msg;
+        console.log(msg);
+        io.emit('message', `${formatDate} ${nickname}: ${chatMessage}`);
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
