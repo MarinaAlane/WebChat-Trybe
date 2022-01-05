7ed860a09afd413fd8f5fb01b6b230d6396bac19
Matheus <matheusaraujo.dev@gmail.com>
2022-01-05 15:50:19 -0300

    Adiciona req 3

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -4,13 +4,21 @@ const { getAll, saveMessage } = require('../models/chatModel');
 const router = Router();
 
 router.get('/webchat', async (_req, res) => {
-  const webchat = await getAll();
-  return res.status(200).json(webchat);
+  try {
+    const webchat = await getAll();
+    return res.status(200).json(webchat);    
+  } catch (error) {
+    return error.message;
+  }
 });
 
 router.post('/', async (req, res) => {
-  const webchat = await saveMessage(req.body);
-  return res.status(200).json(webchat);
+  try {
+    const webchat = await saveMessage(req.body);
+    return res.status(200).json(webchat);    
+  } catch (error) {
+    return error.message;
+  }
 });
 
 module.exports = router;
