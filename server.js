3316d0fec4ba42f5da683eff9beda2e0c8ae0d56
deltadigo <deltadigo@gmail.com>
2022-01-05 09:33:44 -0300

    feat: setup express, socket.io and .env

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-// Faça seu código aqui
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000/',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+server.listen(PORT, () => {
+  console.log(`Servidor online na porta ${PORT}`);
+});
