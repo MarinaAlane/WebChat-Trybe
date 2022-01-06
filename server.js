0c761042c895b4687016b530deb4a953e452787d
Diego Ribeiro <dihrm2001@gmail.com>
2022-01-06 13:24:07 -0300

    Push para atualizar o PR

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
+// Atualiza PR :P
 const app = express();
 const http = require('http').createServer(app);
 
