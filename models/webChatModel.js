5ece0998c2164a13523f706d14fc0179f9e3d276
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-01-06 00:16:20 -0300

    fix requirement two and done requirement three

---
+++ models/webChatModel.js
@@ -0,0 +1,20 @@
+const connection = require('./connection');
+
+const getMessage = async () => {
+  const dbConnection = await connection();
+  const result = await dbConnection.collection('messages').find().toArray();
+  return result;
+};
+
+const postMessage = async ({ dateNow, nickname, chatMessage }) => {
+  const dbConnection = await connection();
+  const result = await dbConnection.collection('messages').insertOne({
+    dateNow, nickname, chatMessage,
+  });
+  return result;
+};
+
+module.exports = {
+  getMessage,
+  postMessage,
+}; 
