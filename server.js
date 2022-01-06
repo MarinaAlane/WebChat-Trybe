7bdf93e7b2b0cf6ce3c70eb9358b13651690d8a6
cristian <cristian.brum2@yahoo.com.br>
2022-01-06 02:52:12 -0300

    feat: :rocket: add req 4

--- server.js
+++ server.js
@@ -17,27 +17,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+const nick = {};
 io.on('connection', async (socket) => {
-  console.log(`Usuário ${socket.id} conectou`);
+  console.log(`Usuário: ${socket.id} conectado!`); nick[socket.id] = socket.id.slice(0, 16);
+
+  socket.on('disconnect', () => {
+    console.log(`Usuário: ${socket.id} está desconectado!`);
+    delete nick[socket.id]; io.emit('onlineUser', Object.values(nick));
+  });
 
   socket.on('message', async ({ chatMessage, nickname }) => {
     const date = moment();
     const formatedDate = date.format('DD-MM-yyyy hh:mm:ss A');
 
     await Chat.create({
-      message: chatMessage,
-      nickname,
-      timestamp: formatedDate,
-    });
+      message: chatMessage, nickname, timestamp: formatedDate });
     io.emit('message', `${formatedDate} - ${nickname}: ${chatMessage}`);
   });
 
-  socket.emit('history', await Chat.getAll());
-
-  socket.on('disconnect', () => {
-    console.log(`Usuário ${socket.id} desconectou`);
+  socket.on('newUserName', (nickname) => {
+    nick[socket.id] = nickname; io.emit('onlineUser', Object.values(nick));
   });
+  
+  socket.emit('history', await Chat.getAll());
+  io.emit('onlineUser', Object.values(nick));
 });
 
 const PORT = process.env.PORT || 3000;
