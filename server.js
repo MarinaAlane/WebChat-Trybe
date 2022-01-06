bb16cd1a0ab47f7629e9e918294113cf3695de3f
deltadigo <deltadigo@gmail.com>
2022-01-06 11:55:35 -0300

    feat: Requisito 4

--- server.js
+++ server.js
@@ -24,22 +24,69 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+const onlineClientsList = [];
+
+const createOnlineClient = (nickname, socketId) => {
+  const onlineClient = {
+    nickname,
+    socketId,
+  };
+  return onlineClient;
+};
+
+const formatMessage = async (nickname, chatMessage) => {
+  const moment = Moment();
+  const formatedDate = moment.format('DD-MM-yyyy hh:mm:ss A');
+  await messages.insertMessage({ nickname, message: chatMessage, timestamp: formatedDate });
+  const formatedMessage = `${formatedDate} - ${nickname}: ${chatMessage}`;
+  return formatedMessage;
+};
+
+const ioEmitOnlineClientsList = (ioSocket) => {
+  ioSocket.emit('online-clients', onlineClientsList);
+};
+
+const updateNicknameInList = (nickname, socket, ioSocket) => {
+  for (let index = 0; index < onlineClientsList.length; index += 1) {
+    if (onlineClientsList[index].socketId === socket.id) {
+      onlineClientsList[index].nickname = nickname;
+    }
+  }
+  ioEmitOnlineClientsList(ioSocket);
+};
+
+const removeUserOnline = (socket, ioSocket) => {
+  for (let index = 0; index < onlineClientsList.length; index += 1) {
+    if (onlineClientsList[index].socketId === socket.id) {
+      onlineClientsList.splice(index, 1);
+    }
+  }
+  ioEmitOnlineClientsList(ioSocket);
+};
+
 io.on('connection', async (socket) => {
   const randomNickname = socket.id.slice(-16);
-
   socket.emit('randomNickname', randomNickname);
+
+  const onlineClient = createOnlineClient(randomNickname, socket.id);
+
+  onlineClientsList.push(onlineClient);
+  
+  socket.on('updateNickname', (nickname) => {
+    updateNicknameInList(nickname, socket, io);
+  });
+
+  socket.on('disconnect', () => {
+    removeUserOnline(socket, io);
+  });
+  ioEmitOnlineClientsList(io);
 
   const previousMessages = await messages.getAllMessages();
 
   socket.emit('previousMessages', previousMessages);
 
   socket.on('message', async ({ nickname, chatMessage }) => {
-    const moment = Moment();
-    const formatedDate = moment.format('DD-MM-yyyy hh:mm:ss A');
-    await messages.insertMessage({ nickname, message: chatMessage, timestamp: formatedDate });
-
-    const formatedMessage = `${formatedDate} - ${nickname}: ${chatMessage}`;
-  
+    const formatedMessage = formatMessage(nickname, chatMessage);  
     io.emit('message', formatedMessage);
   });
 });
