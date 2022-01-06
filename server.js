08e9cfc5c1d750da21df6c05e4c2a0a3d1506bac
Guilherme Pereira <guilhermepdo1508@gmail.com>
2022-01-05 23:36:03 -0300

    feat: Feito HTML

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
+});
+
+require('./sockets/message')(io);
+
+app.use(express.static(`${__dirname}/views`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
