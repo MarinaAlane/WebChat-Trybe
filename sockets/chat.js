3a8b6789219d528363fca40e051afc67789e77aa
dcmatheus <mathws.costag@gmail.com>
2022-01-06 12:15:37 -0300

    .

--- sockets/chat.js
+++ sockets/chat.js
@@ -4,6 +4,7 @@ module.exports = (io) => io.on('connection', (socket) => {
   socket.on('message', async ({ chatMessage: message, nickname }) => {
     const timestamp = new Date().toLocaleString('pt-BR').replaceAll('/', '-').replaceAll(',', '');
     await service.addMessage({ message, nickname, timestamp });
+
     io.emit('message', `${timestamp} - ${nickname}: ${message}`);
   });
 });
