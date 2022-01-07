ca5e7ddc28eefcc8af945a2f10bb4f869c29d2e1
Ivaldo Souza <ivaldo.souza@datapar.com.br>
2022-01-06 21:07:45 -0300

    req4

--- socket/chat.js
+++ socket/chat.js
@@ -1,16 +1,29 @@
 const moment = require('moment');
 
-module.exports = (io) => io.on('connection', (socket) => {
-  console.log(`Usuário ${socket.id} conectou`);
-  
-  const date = moment(new Date()).format('DD-MM-YYYY, h:mm:ss');
+const date = moment(new Date()).format('DD-MM-YYYY, h:mm:ss');
 
-  const message = ({ chatMessage, nickname }) =>
-  `${date} - ${nickname}: ${chatMessage}`;
+const message = ({ chatMessage, nickname }) => `${date} - ${nickname}: ${chatMessage}`;
+
+let users = [];
+
+module.exports = (io) => io.on('connection', (socket) => {
+  console.log(`Usuário ${socket.handshake.query.nick} conectou`);
+
+  users.push({ id: socket.id, nick: socket.handshake.query.nick });
+  io.emit('users', users);
   
   socket.on('message', (data) => {
     io.emit('message', message(data));
   });
 
-  socket.on('disconnect', () => console.log(`Usuário ${socket.id} desconectou`));
+  socket.on('users', (name) => {
+    const index = users.findIndex(({ id }) => id === socket.id);
+    users[index].nick = name;
+    socket.broadcast.emit('users', users);
+  });
+
+  socket.on('disconnect', () => {
+    users = users.filter(({ id }) => id !== socket.id);
+    io.emit('users', users);
+  });
 });
