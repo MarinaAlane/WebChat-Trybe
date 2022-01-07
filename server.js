623e65b414c07d22a171c844332bb3bc5817790f
Rubia <rubiaemilly29@gmail.com>
2022-01-07 19:43:54 -0300

    iniciando o projeto

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-// Faça seu código aqui
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+const server = http.createServer(app);
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+server.listen(PORT, () => console.log(`Funicionando na porta ${PORT}`));
