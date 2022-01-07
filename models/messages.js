f57b9891fb6794a7a77d3a6ffe428d852eac418d
Gabriel Castro <kastro.gabriel17@gmail.com>
2022-01-07 19:38:37 -0400

    create message

--- models/messages.js
+++ models/messages.js
@@ -1,12 +1,23 @@
 const connection = require('./connection');
 
 const getAllMessages = async () => {
-  const messages = await connection();
-  const result = await messages.collectiop('messages')
-    .find({}, { projection: { _id: 0 } }).toArray();
+  const db = await connection();
+  const result = await db
+    .collection('messages')
+    .find({}, { projection: { _id: 0 } })
+    .toArray();
   return result;
+};
+
+const createMessage = async (message) => {
+  const db = await connection();
+  const result = await db
+    .collection('messages')
+    .insertOne(message);
+  return result.ops[0];
 };
 
 module.exports = {
   getAllMessages,
+  createMessage,
 };
