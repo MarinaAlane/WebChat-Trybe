5cf18d6eb96aeb91c3c3772235e30f29e6ab2da1
Bruna Campos <92brunacampos@gmail.com>
2022-01-06 22:12:37 -0200

    Servidor inicial
    conexão inicial

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+// const { Server } = require('socket.io');
+require('dotenv').config();
+
+const app = express();
+const http = require('http').createServer(app);
+// const io = new Server(server);
+
+const io = require('socket.io')(http, {
+    cors: {
+      origin: 'http://localhost:3000', // url aceita pelo cors
+      methods: ['GET', 'POST'], // Métodos aceitos pela url
+    } });
+  
+  io.on('connection', (socket) => {
+    console.log(`Usuário conectado. ID: ${socket.id} `);
+    io.on('disconnect', () => {
+        console.log(`Usuário desconectado. ID: ${socket.id} `);
+    });
+  });
+
+app.get('/', (_req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = 3000;
+http.listen(PORT, () => console.log(`rodando na porta ${PORT}`));
