0f622a9312e8ba9366af23b027fb53422db2390a
CamilaCode7 <camilafabiabh9@gmail.com>
2022-01-08 10:42:13 -0300

    🔨  commit inicial criando socket 🔨

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app); // servidor HTTP
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
+});
+// Sempre que um cliente se conectar ao servidor executa essa função
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id} `);
+});
+  
+app.get('/', (_req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
+});
