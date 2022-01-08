6c33dd3e349bbd9051da4373f9f238064bec3843
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2022-01-08 07:43:51 -0300

    feat: ensures the API is running on a temporary route

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 // Faça seu código aqui
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.use('/', (_req, res) => res.status(200).json({ message: 'OK' }));
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`API running on port: ${PORT}`));
