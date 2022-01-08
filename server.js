4073cb6c3046ffe05fe9e4ede172eb0f699ab5fb
OldRoge <rogeriomrjr@icloud.com>
2022-01-08 11:46:31 -0300

    build: create server file

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Faça seu código aqui
+const express = require('express');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const PORT = 3000;
+
+const server = require('http').createServer(app);
+
+const io = new Server(server);
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(`${__dirname}, index.html`));
+});
+
+io.on('connection', (socket) => {
+  console.log(`User ${socket.id} connected`);
+});
+
+server.listen(PORT, () => console.log(`Escutando a porta ${PORT}`));
