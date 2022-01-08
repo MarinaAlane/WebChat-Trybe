08f29fd268a9b195c85eaa8c6719fbd8b28b9573
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2022-01-08 15:38:55 -0300

    requisito 1

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-// Faça seu código aqui
+ const express = require('express');
+ const cors = require('cors'); 
+
+ const app = express();
+ const http = require('http').createServer(app);
+ const path = require('path');
+
+ const PORT = 3000;
+
+ const io = require('socket.io')(http, {
+   cors: {
+     origin: 'http://localhost:3000', // url aceita pelo cors
+     methods: ['GET', 'POST'], // Métodos aceitos pela url
+   },
+ });
+
+ require('./socket')(io); 
+
+ app.use(cors()); 
+
+ app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+  }); 
+
+ http.listen(PORT, () => {
+   console.log(`Servidor ouvindo na porta ${PORT}`);
+});
---
+++ socket.js
@@ -0,0 +1,9 @@
+const moment = require('moment');
+
+module.exports = (io) => io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id} `);
+  const dateFormat = moment().format('DD-MM-yyyy hh:mm:ss A');
+  socket.on('message', ({ chatMessage, nickname }) => {
+    io.emit('message', `${dateFormat} - ${nickname}:${chatMessage}`);
+  });
+});
