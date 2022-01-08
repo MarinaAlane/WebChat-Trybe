a99f5de3b1bdda3caf43ad3df5fc7ec6e92a5b08
UmberBn <humberto-bandeira@hotmail.com>
2022-01-08 16:49:22 -0400

    req01

--- models/connection.js
+++ models/connection.js
@@ -5,16 +5,15 @@ let schema = null;
 
 async function connection() {
   if (schema) return Promise.resolve(schema);
-  return MongoClient
-    .connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+  return MongoClient.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
     .then((conn) => conn.db(process.env.DB_NAME))
     .then((dbSchema) => {
       schema = dbSchema;
       return schema;
-    })
+    });
 }
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-// Faça seu código aqui
+const express = require('express');
+const moment = require('moment');
+
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
+});
+const userRoutes = require('./routes/user.routes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/', userRoutes);
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id} `);
+  
+  socket.on('message', ({ nickname, chatMessage }) => {
+    const currentDate = moment().format('DD-MM-YYYY HH:MM:ss A');
+    const changeNickName = nickname || socket.id;
+    io.emit('message', `${currentDate} ${changeNickName}: ${chatMessage}`);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
