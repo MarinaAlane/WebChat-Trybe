296207b89e7f111a46ec281b0e210737683d1e22
Gabriel Castro <kastro.gabriel17@gmail.com>
2022-01-07 21:49:51 -0400

    serve and socket message and router

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+// Retirado dos ensinamentos de Socket.io na Trybe.
 const express = require('express');
-const cors = require('cors');
+const CORS = require('cors');
+const path = require('path');
 
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const router = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+require('./sockets')(io);
 
-app.listen(PORT, () => console.log(`rodando na porta - ${PORT}`));
+app.use(CORS());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'ejs');
+
+app.use('/', router);
+
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
