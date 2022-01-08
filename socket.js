4df3f33d53e9eac3d033999b8e5f04e8b767b920
Bruna Campos <92brunacampos@gmail.com>
2022-01-08 21:03:37 -0200

    requisito 1 quebrando

--- socket.js
+++ socket.js
@@ -1,16 +1,24 @@
 const moment = require('moment');
 
+const onLineUsers = {};
+
 module.exports = (io) => io.on('connection', (socket) => {
     console.log(`Usuário conectado. ID: ${socket.id} `);
+
+    const { id } = socket;
+    const nickId = id.substring(0, 16);
+
+    onLineUsers[socket.id] = nickId;
+    io.emit('sendNick', onLineUsers[socket.id]);
     
     const currentDate = moment().format('DD-MM-yyyy hh:mm:ss A');
 
     socket.on('message', ({ nickname, chatMessage }) => {
-        io.emit('message', `${currentDate} - ${nickname}: ${chatMessage}`);
+        io.emit('message', ` ${currentDate} - ${onLineUsers[nickname]}: ${chatMessage}`); 
     }); 
-    
-    const { id } = socket;
-    const nickname = id.substring(0, 16);
 
-    io.emit('nickSapo', nickname);
+    socket.on('sendNickName', (nick) => {
+        onLineUsers[socket.id] = nick;
+        io.emit('newNickName', onLineUsers);
+    });
 });
