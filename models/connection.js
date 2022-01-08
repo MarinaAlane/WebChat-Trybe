1bb7c25d8ed7a567b388e6c91c94b457468678ee
nsilva90 <nikolas.silva1990@gmail.com>
2022-01-08 12:57:46 -0300

    Initial socket configs done

--- models/connection.js
+++ models/connection.js
@@ -20,5 +20,4 @@ async function connection() {
     });
 }
 
-
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const http = require('http');
-const server = http.createServer(app);
+const path = require('path');
+const { Server } = require('socket.io');
 
 const app = express();
 const PORT = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (_req, res) => {
-    res.status(400).json({ message: 'Working!' })
-})
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log(`User ${socket.id} connected`);
+    socket.on('disconnect', () => {
+        console.log(`User ${socket.id} disconnected`);
+    });
+});
 
 server.listen(PORT, () => console.log(`Server conected on port ${PORT}!`));
