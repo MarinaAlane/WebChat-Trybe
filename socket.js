2e7e460001da04db87a7e7a8627bfb102ae92cc1
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2022-01-08 20:16:02 -0300

    requisito 2

--- socket.js
+++ socket.js
@@ -1,9 +1,24 @@
 const moment = require('moment');
 
+const online = {};
+
 module.exports = (io) => io.on('connection', (socket) => {
   console.log(`Usuário conectado. ID: ${socket.id} `);
+
+  const { id } = socket;
+  const nicks = id.substring(0, 16);
+
+  online[socket.id] = nicks;
+  io.emit('sendNickname', online[socket.id]);
+
   const dateFormat = moment().format('DD-MM-yyyy hh:mm:ss A');
+
   socket.on('message', ({ chatMessage, nickname }) => {
-    io.emit('message', `${dateFormat} - ${nickname}:${chatMessage}`);
+    io.emit('message', `${dateFormat} - ${online[nickname]}:${chatMessage}`);
+  });
+  
+  socket.on('sendNickname', (nick) => {
+    online[socket.id] = nick;
+    io.emit('newNickname', online);
   });
 });
