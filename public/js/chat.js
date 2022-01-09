8423469cbfb8bfd687c931236a02aec75c1b48d7
Marconi <marconieng@gmail.com>
2022-01-09 16:24:00 -0300

    Adiciona requisito 2

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,32 +1,41 @@
 const socket = window.io();
 
+const onLineUser = (user) => {
+  const userOnLineUl = document.querySelector('#client');
+  const li = document.createElement('li');
+  li.setAttribute('data-testid', 'online-user');
+  li.innerText = user;
+  userOnLineUl.appendChild(li);
+};
+
+socket.on('user', (user) => onLineUser(user));
+
 const formClient = document.querySelector('#client-form');
 const inputNickname = document.querySelector('#clientInput');
 
 formClient.addEventListener('submit', (e) => {
   e.preventDefault();
-  socket.emit('nickname', inputNickname.value);
+  sessionStorage.setItem('nickname', inputNickname.value);
+  const userNickName = sessionStorage.getItem('nickname');
+  socket.emit('userNick', userNickName);
+  const userOnLineUl = document.querySelector('#client');
+  while (userOnLineUl.firstChild) {
+    userOnLineUl.removeChild(userOnLineUl.firstChild);
+  }
+  onLineUser(userNickName);
   inputNickname.value = '';
   return false;
 });
-
-const saveNickname = (nickname) => {
-  const clientNameUl = document.querySelector('#client');
-  const li = document.createElement('li');
-  li.innerText = nickname;
-  clientNameUl.appendChild(li);
-};
-
-socket.on('nickname', (nickname) => saveNickname(nickname));
 
 const formMessages = document.querySelector('#form-messages');
 const inputMessage = document.querySelector('#messageInput');
 
 formMessages.addEventListener('submit', (e) => {
+  const nickName = socket.id.substring(0, 16);
   e.preventDefault();
   socket.emit('message', {
     chatMessage: inputMessage.value,
-    nickname: socket.id,
+    nickname: nickName,
   });
   inputMessage.value = '';
   return false;
@@ -35,6 +44,7 @@ formMessages.addEventListener('submit', (e) => {
 const createMessage = (message) => {
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
+  li.setAttribute('data-testid', 'message');
   li.innerText = message;
   messagesUl.appendChild(li);
 };
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,5 +1,11 @@
 module.exports = (io) => io.on('connection', (socket) => {
-  socket.broadcast.emit('serverMessage', console.log(`${socket.id} acabou de se conectar :D`));
+  let userName = socket.id.substring(0, 16);
+
+  io.emit('user', `${userName}`);
+
+  socket.on('userNick', (user) => {
+    userName = user;
+  });
 
   // https://stackoverflow.com/questions/10632346/how-to-format-a-date-in-mm-dd-yyyy-hhmmss-format-in-javascript
   socket.on('message', (message) => {
@@ -7,10 +13,6 @@ module.exports = (io) => io.on('connection', (socket) => {
     const dateFormat = [date.getDate(), date.getMonth() + 1, date.getFullYear()].join('-');
     const hourFormat = [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
 
-    io.emit('message', `${dateFormat} ${hourFormat} - ${message.nickname}: ${message.chatMessage}`);
-  });
-
-  socket.on('nickname', (nickname) => {
-    io.emit('nickname', nickname);
+    io.emit('message', `${dateFormat} ${hourFormat} - ${userName}: ${message.chatMessage}`);
   });
 });
