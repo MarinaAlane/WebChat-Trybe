f531eec1fd14256a846e28aa55cb6fd744723e01
Henrique Pozzolini <hpmoreira05@gmail.com>
2022-01-09 18:45:18 -0300

    Ch

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,11 +1,11 @@
 const moment = require('moment');
 const chatController = require('../controllers/chatController');
 
-const date = moment().format('MM-DD-YYYY h:mm:ss A');
+const timestamp = moment().format('MM-DD-YYYY h:mm:ss A');
 module.exports = (io) => io.on('connection', (socket) => {
-  socket.on('message', async ({ chatMessage, nickname }) => {
-    await chatController.create({ chatMessage, nickname, date });
-    io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
+  socket.on('message', async ({ chatMessage: message, nickname }) => {
+    await chatController.create({ message, nickname, timestamp });
+    io.emit('message', `${timestamp} - ${nickname}: ${message}`);
   });
 
   socket.on('nickname', () => {
