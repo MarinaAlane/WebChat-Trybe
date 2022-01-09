172fc4db416a70b32c16912cfcb84b500dd7db82
Thiago Gomes <thiagog.macedom@gmail.com>
2022-01-09 19:51:07 -0300

    add - task 2

--- public/js/main.js
+++ public/js/main.js
@@ -3,23 +3,32 @@ const messages = document.querySelector('#messages');
 const form = document.querySelector('#form');
 const inputNickname = document.querySelector('#input-nickname');
 const inputChat = document.querySelector('#input-chat');
-
-console.log('auau');
+const buttonChangeNickName = document.querySelector('#button-change-nickname');
+const nickname = document.querySelector('#nickname');
+const randomNickNameLength = 16;
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (inputChat.value) {
     socket.emit('message', {
-      chatMessage: inputChat.value, nickname: inputNickname.value,
+      chatMessage: inputChat.value, nickname: nickname.innerText,
     });
 
     inputChat.value = '';
   }
 });
+/* eslint no-bitwise: [2, { allow: ["~"] }] */
+nickname.innerText = [...Array(randomNickNameLength)]
+  .map((_i) => (~~(Math.random() * 36)).toString(36)).join('');
 
-socket.on('message', (msg) => {
+buttonChangeNickName.addEventListener('click', () => {
+  nickname.innerText = inputNickname.value;
+});
+
+  socket.on('message', (msg) => {
   const newMessage = document
     .createElement('li');
+    newMessage.setAttribute('data-testid', 'message');
   newMessage.innerText = msg;
   messages.appendChild(newMessage);
 });
