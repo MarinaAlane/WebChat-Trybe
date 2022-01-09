b25f0fe528547b0e4daac5faaabc060013b60a39
DarioJunior <dario.junior.rs@gmail.com>
2022-01-09 15:19:29 -0300

    Improve: cria uma funçao para a geracao do id do user

--- sockets/chat.js
+++ sockets/chat.js
@@ -5,10 +5,21 @@ const ModelMessage = require('../models/messageHistory');
 
 let usersOnline = [];
 
-module.exports = (io) => io.on('connection', (socket) => {
-  let userNickname = socket.id.slice(0, 16);
-  usersOnline.push(userNickname);
+const generateUserId = (socket) => {
+  const userId = socket.id.slice(0, 16);
+  usersOnline.push(userId);
+  return userId;
+};
 
+const renderMessageHistory = () => {
+
+};
+
+module.exports = (io) => io.on('connection', (socket) => {
+  let userNickname = generateUserId(socket);
+  // [ ] - Criar um evento para renderizar o histórico de mensagens;
+  renderMessageHistory(io, socket);
+  
   // [X] 1º - Remover o envio do nickName e fazer um evento próprio;
   io.emit('userConnected', usersOnline);
   socket.emit('setUserId', { userNickname, usersOnline });
