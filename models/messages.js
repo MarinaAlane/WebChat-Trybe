7214d1c3e26bf7c2c82062ee6e6478f9809623a1
CamilaCode7 <camilafabiabh9@gmail.com>
2022-01-09 10:29:05 -0300

    criando getAll no models

--- models/messages.js
+++ models/messages.js
@@ -6,6 +6,13 @@ const create = async (data, nickname, chatMessage) => {
   return result;
 };
 
+const getAll = async () => {
+  const db = await connection();
+  const result = await db.collection('messages').find().toArray();
+  return result;
+};
+
 module.exports = {
-  create,  
+  create,
+  getAll,
 };
