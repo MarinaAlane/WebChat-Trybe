2069cdafdb2d20a89739bb086ee9fccf6ed89be0
giovani.diccini <giovani.diccini@gmail.com>
2022-01-09 18:56:07 -0300

    req 2

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,14 @@
 const socket = window.io();
 
-const form = document.querySelector('form');
+let nickname = '';
+
+// message area
+const sendBtn = document.querySelector('#sendBtn');
 const inputMessage = document.querySelector('#messageInput');
 
-form.addEventListener('submit', (e) => {
+sendBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  socket.emit('message', { chatMessage: inputMessage.value, nickname: socket.id });
+  socket.emit('message', { chatMessage: inputMessage.value, nickname });
   inputMessage.value = '';
   return false;
 });
@@ -14,7 +17,43 @@ const createMessage = (message) => {
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
   li.innerText = message;
+  li.setAttribute('data-testid', 'message');
   messagesUl.appendChild(li);
 };
 
 socket.on('message', (message) => createMessage(message));
+
+// add user area
+const createUsers = (users) => {
+  const usersList = document.querySelector('#usersList');
+  while (usersList.lastElementChild) {
+    usersList.removeChild(usersList.lastElementChild);
+  }
+
+  users.forEach((username) => {
+    const userLi = document.createElement('li');
+    userLi.innerText = username;
+    usersList.appendChild(userLi);
+  });
+};
+
+socket.on('userConnected', ({ userList, username }) => {
+  nickname = username;
+  createUsers(userList);
+});
+
+// change nickname
+const nickInput = document.querySelector('#nickInput');
+const nickBtn = document.querySelector('#nickBtn');
+
+nickBtn.addEventListener('click', () => {
+  if (nickInput.value) {
+    nickname = nickInput.value;
+    socket.emit('changeNick', nickInput.value);
+
+    nickInput.value = '';
+  }
+  return false;
+});
+
+socket.on('nickChanged', (users) => createUsers(users));
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,8 +1,24 @@
+const userList = [];
+
 module.exports = (io) => io.on('connection', (socket) => {
+  let username = socket.id.slice(0, 16); userList.push(username);
+  io.emit('userConnected', { userList, username });
+
   socket.on('message', ({ chatMessage, nickname }) => {
     const date = new Date().toLocaleString('pt-BR');
     const formatedDate = date.replace(/\//g, '-');
-    console.log(formatedDate);
     io.emit('message', `${formatedDate} ${nickname}: ${chatMessage}`);
+  });
+
+  socket.on('changeNick', (newNick) => {
+    const index = userList.indexOf(username); username = newNick;
+    userList[index] = username;
+    io.emit('nickChanged', userList);
+  });
+
+  socket.on('disconnect', () => {
+    const index = userList.indexOf(username);
+    userList.splice(index, 1);
+    io.emit('userDisconnected', userList);
   });
 });
