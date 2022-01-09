54bfc122fb157d5cc8060de503091e94d4fd87ae
Pedro Piquini <piquini.pedro@gmail.com>
2022-01-09 18:14:42 -0300

    Começando o projeto

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-// Faça seu código aqui
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    io.emit('message', msg);
+  });
+});
+
+server.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
