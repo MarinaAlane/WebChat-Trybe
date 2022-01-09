d8b48c1f70c25d6c364cf5949e844e7767709901
FredericoCanoeira <fcanoeira@gmail.com>
2022-01-09 23:22:56 +0100

    Implementing Req02

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,53 @@
 const socket = window.io();
 
-socket.emit('message', {
-    nickname: 'Freddy',
-    chatMessage: 'Web Chat Project',
+const users = document.getElementById('users');
+const messages = document.getElementById('messages');
+const form = document.getElementById('form');
+const messageBox = document.getElementById('message-box');
+const nicknameBox = document.getElementById('nickname-box');
+const nicknameButton = document.getElementById('nickname-button');
+
+let nickname = '';
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  socket.emit('message', {
+    nickname,
+    chatMessage: messageBox.value,
+  });
+  messageBox.value = '';
 });
+
+nicknameButton.addEventListener('click', () => {
+  nickname = nicknameBox.value;
+  nicknameBox.value = '';
+  socket.emit('saveNickname', nickname);
+});
+
+const createnewMessage = (message) => {
+  const li = document.createElement('li');
+  li.innerText = message;
+  li.setAttribute('data-testid', 'message');
+  messages.appendChild(li);
+};
+
+const createUser = (newNickname) => {
+  nickname = newNickname;
+  socket.emit('usersOnline');
+};
+
+const setUserList = (userList) => {
+  users.innerHTML = '';
+  const clientUser = userList.find((user) => user.id === socket.id);
+  const li = document.createElement('li');
+  li.innerText = clientUser.nickname;
+  li.setAttribute('data-testid', 'online-user');
+  users.appendChild(li);
+};
+
+socket.on('message', createnewMessage);
+socket.on('newUser', createUser);
+socket.on('usersOnline', setUserList);
 
 socket.on('message', (msg) => {
    console.log(msg);
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,14 +1,40 @@
 const moment = require('moment');
 
-const msgSend = (io, socket) => {
-    const timestamp = moment().format('DD-MM-YY h:mm:ss A');
-    socket.on('message', ({ nickname, chatMessage }) => {
-      io.emit('message', `${timestamp} ${nickname} ${chatMessage}`);      
-    });
+const usrsOnline = [];
+
+const createUser = (socket) => {
+  const { id } = socket;
+  const newId = id.substr(0, 16);
+  usrsOnline.push({ id, nickname: newId });
+  socket.emit('newUser', newId);
+};
+
+const newMsg = (socket, io) => {
+  const timestamp = moment().format('DD-MM-YYYY h:mm:ss A');
+  socket.on('message', ({ nickname, chatMessage }) => {
+    io.emit('message', `${timestamp} - ${nickname}: ${chatMessage}`);
+  });
+};
+
+const newNickname = (socket, io) => {
+  socket.on('saveNickname', (newNick) => {
+    const userIndex = usrsOnline.findIndex((user) => user.id === socket.id);
+    usrsOnline[userIndex].nickname = newNick;
+    io.emit('usrsOnline', usrsOnline);
+  });
+};
+
+const updateUsersOnline = (socket, io) => {
+  socket.on('usrsOnline', () => {
+    io.emit('usrsOnline', usrsOnline);
+  });
 };
 
 module.exports = (io) => {
-    io.on('connection', (socket) => {
-        msgSend(socket, io);
-      });
-    };
+  io.on('connection', (socket) => {
+    newMsg(socket, io);
+    createUser(socket);
+    newNickname(socket, io);
+    updateUsersOnline(socket, io);
+  });
+};
