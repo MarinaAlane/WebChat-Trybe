33f1060744ec9001f390d3e53bfe7e485c1bcfc2
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2022-01-09 00:02:08 -0300

    req02 em construçao

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,39 @@
 const socket = window.io();
 
-const nickname = 'Leandro';
-const chatMessage = 'Olá à todos';
+const form = document.querySelector('#form');
+const inputName = document.querySelector('#nickname-box');
+const inputMsg = document.querySelector('#message-box');
+const btnSalvar = document.querySelector('#nickname-button');
+const ulMsg = document.querySelector('#message');
+const ulUsers = document.querySelector('#users');
 
-socket.emit('message', { nickname, chatMessage });
-socket.on('message', (msg) => {
-  console.log(msg);
+let nickname = '';
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  socket.emit('message', { nickname, chatMessage: inputMsg.value });
 });
+
+function createUser(user) {
+  const li = document.createElement('li');
+  li.innerText = user;
+
+  li.setAttribute('data-testid', 'online-user');
+  ulUsers.appendChild(li);
+}
+
+btnSalvar.addEventListener('click', () => {
+  nickname = inputName.value;
+});
+
+function createMessage(msg) {
+  const li = document.createElement('li');
+  li.innerText = msg;
+
+  li.setAttribute('data-testid', 'message');
+  ulMsg.appendChild(li);
+}
+
+socket.on('message', createMessage);
+socket.on('user', createUser);
--- socket/chat.js
+++ socket/chat.js
@@ -4,6 +4,13 @@ const socketIo = (io) => {
   io.on('connection', (socket) => {
     console.log('servidor conectado');
 
+    const { id } = socket;
+    const newId = id.substr(0, 16);
+
+    socket.emit('user', newId);
+
+    // socket.on('salvarName');
+
     socket.on('message', ({ nickname, chatMessage }) => {
       const timestamp = moment().format('DD-MM-YYYY h:mm:ss A');
 
