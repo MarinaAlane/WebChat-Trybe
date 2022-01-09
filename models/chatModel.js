deb31e5906d087c1de13c8eadd01adfd083eec2b
Henrique Pozzolini <hpmoreira05@gmail.com>
2022-01-09 18:08:05 -0300

    Change collection

--- models/chatModel.js
+++ models/chatModel.js
@@ -2,14 +2,14 @@ const connection = require('./connection');
 
 const create = async ({ message, nickname, timestamp }) => {
   const db = await connection();
-  const msg = await db.collection('chat')
+  const msg = await db.collection('messages')
     .insertOne({ message, nickname, timestamp });
   return msg;
 };
 
 const getMessages = async () => {
   const db = await connection();
-  const chat = await db.collection('chat').find().toArray();
+  const chat = await db.collection('messages').find().toArray();
   return chat;
 };
 
