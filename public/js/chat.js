db23afc319fbd7807e7eb6647cac776762809da6
FredericoCanoeira <fcanoeira@gmail.com>
2022-01-09 20:46:51 +0100

    Implementing Req01

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,10 @@
 const socket = window.io();
+
+socket.emit('message', {
+    nickname: 'Freddy',
+    chatMessage: 'Web Chat Project',
+});
+
+socket.on('message', (msg) => {
+   console.log(msg);
+});
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,5 +1,14 @@
-const socketIo = (io) => {
-    io.on('connection', () => {
-        console.log('servidor conectado')
-    }),
-}
+const moment = require('moment');
+
+const msgSend = (io, socket) => {
+    const timestamp = moment().format('DD-MM-YY h:mm:ss A');
+    socket.on('message', ({ nickname, chatMessage }) => {
+      io.emit('message', `${timestamp} ${nickname} ${chatMessage}`);      
+    });
+};
+
+module.exports = (io) => {
+    io.on('connection', (socket) => {
+        msgSend(socket, io);
+      });
+    };
