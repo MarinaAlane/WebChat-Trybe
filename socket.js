9862275dc03b677fa0c5d0e93ad944c9ee2d9778
gabiazevedo <gabiazevedoms@gmail.com>
2022-01-08 22:18:30 -0300

    fix: Corrige req nickname req 01

--- socket.js
+++ socket.js
@@ -1,25 +1,24 @@
 // conexão aberta com o back-end
 const moment = require('moment');
-
 const onLineUsers = {};
 
 module.exports = (io) => io.on('connection', (socket) => {
   console.log(`Usuário conectado. ID: ${socket.id} `);
 
   const { id } = socket;
-  const randomNickName = id.substring(0, 16);
+  const nickname = id.substring(0, 16);
   
-  onLineUsers[socket.id] = randomNickName;
+  onLineUsers[socket.id] = nickname;
   io.emit('sendNickName', onLineUsers[socket.id]);
 
   const dateFormat = moment().format('DD-MM-yyyy hh:mm:ss A');
 
   socket.on('message', ({ chatMessage, nickname }) => {
-    io.emit('message', ` ${dateFormat} - ${onLineUsers[nickname]}: ${chatMessage}`); 
+    io.emit('message', ` ${dateFormat} - ${nickname}: ${chatMessage}`); 
   });
 
   socket.on('sendNickName', (nick) => {
     onLineUsers[socket.id] = nick;
-    io.emit('newNickName', onLineUsers);
+    io.emit('newNickName', onLineUsers)
   });
 });
