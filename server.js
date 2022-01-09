d2b71fc0ec8f2811b77cdff74b2a5375788396e6
LeoLacana <wilson17414@gmail.com>
2022-01-09 02:08:19 -0300

    Requisito 1 Done

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-// Faça seu código aqui
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+const moment = require('moment');
+
+const app = express();
+const PORT = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'chat.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log(`usuário ${socket.id} conectado`);
+  const dateFormat = moment().format('DD-MM-yyyy hh:mm:ss A');
+  socket.on('message', ({ nickname, chatMessage }) => {
+    io.emit('message', `${dateFormat} - ${nickname}:${chatMessage}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`usuário ${socket.id} desconectou`);
+  });
+});
+server.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
