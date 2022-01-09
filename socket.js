5388ee3f42fa42459047de2fe85799a41b84a1c5
gabiazevedo <gabiazevedoms@gmail.com>
2022-01-08 22:23:53 -0300

    fix: Corrige erros lint

--- socket.js
+++ socket.js
@@ -1,14 +1,15 @@
 // conexão aberta com o back-end
 const moment = require('moment');
+
 const onLineUsers = {};
 
 module.exports = (io) => io.on('connection', (socket) => {
   console.log(`Usuário conectado. ID: ${socket.id} `);
 
   const { id } = socket;
-  const nickname = id.substring(0, 16);
+  const newNick = id.substring(0, 16);
   
-  onLineUsers[socket.id] = nickname;
+  onLineUsers[socket.id] = newNick;
   io.emit('sendNickName', onLineUsers[socket.id]);
 
   const dateFormat = moment().format('DD-MM-yyyy hh:mm:ss A');
@@ -19,6 +20,6 @@ module.exports = (io) => io.on('connection', (socket) => {
 
   socket.on('sendNickName', (nick) => {
     onLineUsers[socket.id] = nick;
-    io.emit('newNickName', onLineUsers)
+    io.emit('newNickName', onLineUsers);
   });
 });
