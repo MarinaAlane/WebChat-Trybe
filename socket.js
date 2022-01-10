6c8b8a6f95fddae4add0fe75481c46396319a622
Junior Portela <portelajr14@gmail.com>
2022-01-10 01:43:52 -0300

    feat: requisito 3

--- socket.js
+++ socket.js
@@ -1,18 +1,23 @@
 const moment = require('moment');
+const chatModel = require('./models/chatModel');
 
 const online = {};
 
-module.exports = (io) => io.on('connection', (socket) => {
+module.exports = (io) => io.on('connection', async (socket) => {
   console.log(`Usuário de ID: ${socket.id} conectado! `);
-  const date = moment().format('DD-MM-yyyy hh:mm:ss A');
-
+  
   const { id } = socket;
   const nicks = id.substring(0, 16);
 
   online[socket.id] = nicks;
   io.emit('sendNickname', online[socket.id]);
+  
+  const allMessages = await chatModel.listMessages();
+  io.emit('allMessages', allMessages);
 
   socket.on('message', ({ chatMessage, nickname }) => {
+    const date = moment().format('DD-MM-yyyy hh:mm:ss A');
+    chatModel.createMessage({ message: chatMessage, nickname, date });
     io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
   });
 
