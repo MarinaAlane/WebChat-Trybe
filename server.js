4318c6081b70f84dff17608dcb164e4e095a52c1
Gabriela Feijo <feijo.gabriela@gmail.com>
2022-01-10 13:06:57 -0300

    add 04

--- server.js
+++ server.js
@@ -21,28 +21,40 @@ app.get('/', (req, res) => {
 
 const PORT = 3000;
 // adiciona lógica socket direto no server
+const date = moment(new Date()).format('DD-MM-yyyy h:mm:ss a');
+// console.log({ date });
+
+const message = ({ chatMessage, nickname }) =>
+  `${date} - ${nickname}: ${chatMessage}`;
+let users = [];
+
 io.on('connection', (socket) => {
   console.log(`User ${socket.id} connected`);
 
   socket.on('updateNickname', (nickname) => {
     console.log({ nickname });
-    io.emit('updateNickname', nickname);
+    // io.emit('updateNickname', nickname);
+    users = users.filter((client) => client.id !== socket.id); // filtra o id
+    users.push({ nickname, id: socket.id }); // empurra pra dentro da lista e atualiza
+    io.emit('online', users); // emite para os online
   });
 
   socket.on('disconnect', () => {
     console.log(`User ${socket.id} disconnected`);
+    users = users.filter((client) => client.id !== socket.id); // filtra o id e atualiza a lista de usuãrios
+    // https://stackoverflow.com/questions/63168606/socket-io-doesnt-remove-user-object-from-array-after-disconnect
+    io.emit('online', users);
   });
 });
 
 io.on('connection', async (socket) => {
   const messages = await model.getAll();
   socket.emit('history', messages);
-
-  socket.on('message', ({ chatMessage, nickname }) => {
-    const timestamp = moment().format('DD-MM-YYYY HH:mm:ss');
-    model.saveMessage({ message: chatMessage, nickname, timestamp });
-    io.emit('message', `${timestamp} - ${nickname}: ${chatMessage}`);
-  });
+  
+  socket.on('message', (data) => {
+    model.saveMessage(data);
+    io.emit('message', message(data));
+  }); 
 });
 
 http.listen(PORT, () => console.log(`Servidor ouvindo na porta ${PORT}`)); 
