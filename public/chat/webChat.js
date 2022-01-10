d61a019cf696ef5c4fb75e928770797e8d1b1e0e
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2022-01-09 21:29:57 -0300

    Comitão bomba

---
+++ public/chat/webChat.js
@@ -0,0 +1,62 @@
+const socket = window.io();
+
+const msgInput = document.querySelector('.msg__input');
+const sendMsg = document.querySelector('.send__btn');
+const chatMsgUl = document.querySelector('.msg__list');
+const dataTestId = 'data-testid';
+const nickName = document.querySelector('.nick__name');
+const nickBtn = document.querySelector('.nick__btn');
+const nickBox = document.querySelector('.nick__box');
+let NICK = '';
+
+const listMsg = (msg) => {
+  const createList = document.createElement('li');
+  createList.innerText = msg;
+  createList.setAttribute(dataTestId, 'message');
+  chatMsgUl.appendChild(createList);
+};
+
+sendMsg.addEventListener('click', (event) => {
+  event.preventDefault();
+  const nickname = NICK || socket.id.slice(0, 16);
+  socket.emit('message', { chatMessage: msgInput.value, nickname });
+  msgInput.value = '';
+});
+
+nickBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  NICK = nickName.value;
+  socket.emit('newUser', NICK);
+  if (NICK !== '') {
+    socket.emit('newUser', NICK);
+    nickName.value = '';
+  }
+});
+
+const usersList = (users) => {
+  const firstNick = socket.id.slice(0, 16);
+  nickBox.innerHTML = '';
+  users.forEach((user) => {
+    const li = document.createElement('li');
+    li.setAttribute(dataTestId, 'online-user');
+    li.innerText = user;
+    if (user === NICK || user === firstNick) {
+      return nickBox.prepend(li);
+    }
+    nickBox.appendChild(li);
+  });
+};
+
+const renderMessageDB = (mongo) => {
+  mongo.forEach(({ timestamp, nickname, chatMessage }) => {
+  const li = document.createElement('li');
+  li.innerText = `${timestamp} - ${nickname} : ${chatMessage}`;
+  li.setAttribute(dataTestId, 'message');
+  chatMsgUl.appendChild(li);
+});
+};
+
+socket.on('message', (chatMessage) => listMsg(chatMessage));
+socket.on('allUsers', (users) => usersList(users));
+socket.on('memoryMsg', (mongo) => renderMessageDB(mongo));
+socket.on('nickon', (users) => usersList(users));
--- server.js
+++ server.js
@@ -1,1 +1,52 @@
-// Faça seu código aqui
+require('dotenv').config();
+
+const { Server } = require('socket.io');
+const express = require('express');
+const moment = require('moment');
+const path = require('path');
+
+const app = express();
+const http = require('http').createServer(app);
+const { getMessage, getAllMessages } = require('./models/chat');
+
+const timestamp = moment().format('DD-MM-YYYY hh:mm:ss');
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const io = new Server(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const users = {};
+
+io.on('connection', async (socket) => { 
+  users[socket.id] = socket.id.slice(0, 16);
+  socket.on('message', async ({ chatMessage, nickname }) => {
+    io.emit('message', `${timestamp} - ${nickname} : ${chatMessage}`);
+    await getMessage({ timestamp, nickname, chatMessage });
+  });
+  socket.on('newUser', (nickname) => {
+    users[socket.id] = nickname;
+    io.emit('allUsers', Object.values(users));
+  });
+  io.emit('allUsers', Object.values(users));
+
+  io.emit('memoryMsg', await getAllMessages());
+
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+    io.emit('nickon', Object.values(users));
+  });
+});
+
+app.get('/', (_req, res) => res.render('chat/index'));
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log(`Socket online na ${PORT}, acessar: http://localhost:3000`);
+});
