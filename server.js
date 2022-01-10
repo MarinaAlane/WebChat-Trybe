db4055f0c863f88aa2381bd56a5316d61f0ccb42
palenske <viniciuspalenske@gmail.com>
2022-01-09 22:30:48 -0300

    build: pagina de visualizaçao | renderiza chat | muda nicknam

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const http = require('http');
 const express = require('express');
 const { Server } = require('socket.io');
@@ -8,16 +9,32 @@ const server = http.createServer(app);
 const io = new Server(server);
 const PORT = 3000;
 
-io.on('connection', (socket) => {
-  console.log(`usuário ${socket.id} connectado`);
+const users = {};
 
-  socket.on('message', ({ chatMessage, nickname }) => {
+io.on('connection', (socket) => {
+  users[socket.id] = socket.id.slice(0, 16);
+  console.log(`${users[socket.id]} está online`);
+
+  io.emit('users', users);
+
+  socket.on('message', ({ chatMessage, nickname = users[socket.id] }) => {
     io.emit('message', formatMsg(nickname, chatMessage));
   });
 
+  socket.on('changeNick', (newNick) => {
+    const oldNick = users[socket.id];
+    users[socket.id] = newNick;
+    console.log(`${oldNick} mudou o nick para ${users[socket.id]}`);
+  });
+
   socket.on('disconnet', () => {
+    delete users[socket.id];
     console.log(`usuário ${socket.id} desconectou`);
   });
+});
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(`${__dirname}/views/`, 'index.html'));
 });
 
 server.listen(PORT, () => console.log(`Escutando na porta ${PORT}`));
