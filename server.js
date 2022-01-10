753a4994963404880f47f30057cff3611ce50ba7
Junior Portela <portelajr14@gmail.com>
2022-01-09 21:29:57 -0300

    configura arquivo server.js

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-// Faça seu código aqui
-// l
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const PORT = 3000;
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'teste' });
+});
+
+server.listen(PORT, () => console.log(`Estamos online na ${PORT}`));
