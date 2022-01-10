4726e706974b8ba793a3ec71d5dacd95c874a6e7
OldRoge <rogeriomrjr@icloud.com>
2022-01-10 12:23:10 -0300

    refactor: trying to fix

--- socket/chat.js
+++ socket/chat.js
@@ -4,8 +4,9 @@ const { storeMessage, getAllMessages } = require('../models/socketModel');
 
 module.exports = (io) => io.on('connection', async (socket) => {
   console.log(`a user ${socket.id} connected`);
-  socket.emit('newUser', 'Welcome');
-  io.emit('history', await getAllMessages());
+
+  socket.emit('history', await getAllMessages());
+
   socket.on('message', async (message) => {
     console.log(message);
     await storeMessage(message.chatMessage, message.nickname, formatDate());
